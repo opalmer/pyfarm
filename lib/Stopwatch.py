013af87cb661354537c0735a9ddd82f2adbe0e60
Oliver Palmer <opalmer@xps>
2008-11-11 02:42:58 -0500

    Initial commit of Stopwach module

---
+++ lib/Stopwatch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+'''
+AUTHOR: Oliver Palmer
+CONTACT: opalme20@student.scad.edu || (703)725-6544
+INITIAL: Nov 11 2008
+PURPOSE: To provide various timining and stopwatch like functions to python.
+'''
