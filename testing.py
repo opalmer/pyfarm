3648d7bf127a34619084aff349a49347ddf2245d
Oliver Palmer <opalmer@xps>
2008-12-15 02:00:30 -0500

    -Removed compiled python tools -Added cx_freeze -Created RenderFormat Class 	-used to format command line output for renders -Created test class for testing ONLY

--- testing.py
+++ testing.py
@@ -5,8 +5,7 @@ CONTACT: opalme20@student.scad.edu || (703)725-6544
 INITIAL: Dec 9 2008
 PURPOSE: This program is used for testing purposes ONLY, not to publish
 '''
-import os
-from lib.Install import SystemPackages
+from lib.Test import Internal
 
-pkg = SystemPackages('/home/opalmer/build/network/settings/packages', 'nfs', 'client')
-pkg.install()
+a = Internal('hello world')
+a.echo(5)
