bdf7db9d845a48479ebaef339afbd6adc365c038
Oliver Palmer <opalmer@xps>
2008-12-17 00:00:14 -0500

    Some minor testing

---
+++ send.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''
+AUTHOR: Oliver Palmer
+CONTACT: opalme20@student.scad.edu || (703)725-6544
+INITIAL: Dec 9 2008
+PURPOSE: This program is used for testing purposes ONLY, not to publish
+'''
+from lib.Network import Broadcast
+
+send = Broadcast()
+send.send()
