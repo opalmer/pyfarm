d4ad0bb8e5bc89f9b047c79ed603c7c8d7734a15
Oliver Palmer <opalmer@quadro>
2009-01-31 20:55:05 -0500

    Began work on final client program

---
+++ Client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''
+AUTHOR: Oliver Palmer
+CONTACT: opalme20@student.scad.edu || (703)725-6544
+INITIAL: Jan 31 2009
+PURPOSE: To handle and run all client connections
+'''
+
+import os
+import sys
+from lib.Network import *
