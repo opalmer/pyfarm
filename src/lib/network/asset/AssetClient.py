cc0d008b103c5196c362e086c5ccae355d0e141f
U-xps\opalmer <opalmer@xps>
2009-10-15 00:04:24 -0700

    -Restructed project for easy access -Created another possible http asset server (requires sequential testing)

---
+++ src/lib/network/asset/AssetClient.py
@@ -0,0 +1,17 @@
+# AssetClient
+
+# From Python
+import os
+import os.path
+
+# From PyQt
+from PyQt4.QtCore import QThread
+"""
+NOTES:
+Options 1:
+	-Tar or zip the directory, send the link
+Option 2:
+	-Send ALL files links
+
+"""
+urllib.urlretrieve(url[, filename[, reporthook[, data]]])
