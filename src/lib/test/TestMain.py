d9d755779f1083525e9b9cf027aeb60b61cad2af
Oliver Palmer <opalmer@vuquadro>
2010-02-19 02:25:06 -0800

    Added unittest module for future expansion into unit testing

---
+++ src/lib/test/TestMain.py
@@ -0,0 +1,27 @@
+'''
+HOMEPAGE: www.pyfarm.net
+INITIAL: Feb 19 2010
+PURPOSE: To provide a central location to run PyFarm's main test suite'
+
+    This file is part of PyFarm.
+    Copyright (C) 2008-2010 Oliver Palmer
+
+    PyFarm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyFarm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
+'''
+# DOCS FOR UNIT TESTS
+# http://docs.python.org/library/unittest.html#module-unittest
+# http://docs.python.org/library/unittest.html#unittest.main
+# http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/qtest.html
+
+import unittest
