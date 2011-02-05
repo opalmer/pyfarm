cb9eee529bce6533064973bb253503c599a92ac1
Oliver Palmer <oliverpalmer@opalmer.com>
2011-02-05 03:51:21 -0800

    added deprecated and trace function decorators

--- lib/Decorators.py
+++ lib/Decorators.py
@@ -4,6 +4,7 @@ INITIAL: Feb 5 2011
 PURPOSE: To provide a small set of decorators for use within PyFarm.
          Decorators are used for anything from diagnostics to debugging
          and benchmarking.
+SOURCES: http://wiki.python.org/moin/PythonDecoratorLibrary
 
 This file is part of PyFarm.
 Copyright (C) 2008-2011 Oliver Palmer
@@ -20,10 +21,14 @@ GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 import os
 import sys
 import time
+import cProfile
+import warnings
+import linecache
 from threading import Thread
 from functools import wraps
 
@@ -34,9 +39,17 @@ MODULE = os.path.basename(__file__)
 if PYFARM not in sys.path: sys.path.append(PYFARM)
 from lib import Logger
 
-log          = Logger.Logger(MODULE)
-#log.disabled = True
+# disable the logger and bypass tracebacks for files in TRACE_BYPASS
+log          = Logger.Logger(MODULE).disabled = True
 catch22Fail  = None
+TRACE_BYPASS = (
+                    'string.py', 'Logger.py', 'ElementTree.py',
+                    'AsyncFile.py', '<string>', 'stat.py',
+                    'genericpath.py', 'DebugBase.py', 'DebugClientBase.py',
+                    'AsyncIO.py', 'utf_8.py', 'threading.py', 'ntpath.py',
+                    'atexit.py', '__init__.py'
+                )
+
 def catch22(func):
     '''Catch and process all possible errors in the best possible way'''
     def catcher(*args, **kwargs):
@@ -71,6 +84,66 @@ def catch22(func):
 
     return catcher
 
+def deprecated(func):
+    '''
+    Thow a warning and show information about a deprecated function
+    being used
+    '''
+
+    @wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.warn_explicit(
+            "Call to deprecated function %(funcname)s." % {
+                'funcname': func.__name__,
+            },
+            category=DeprecationWarning,
+            filename=func.func_code.co_filename,
+            lineno=func.func_code.co_firstlineno + 1
+        )
+        return func(*args, **kwargs)
+    return new_func
+
+def trace(func):
+    '''Provide line by line traceback for the given function'''
+    def globaltrace(frame, why, arg):
+        if why == "call":
+            return localtrace
+        return None
+
+    def localtrace(frame, why, arg):
+        if why == "line":
+            filename = frame.f_code.co_filename
+            lineNum  = frame.f_lineno
+            basename = os.path.basename(filename)
+
+            if basename not in TRACE_BYPASS:
+                print "%s(%d): %s" % (
+                                        basename, lineNum,
+                                        linecache.getline(filename, lineNum)
+                                      ),
+        return localtrace
+
+    def runTrace(*args, **kwargs):
+        sys.settrace(globaltrace)
+        result = func(*args, **kwargs)
+        sys.settrace(None)
+        return result
+
+    return runTrace
+
+
+def profile(func):
+    '''Profile the given function and print some usefull information'''
+    @wraps(func)
+    def runProfile(*args, **kwargs):
+
+        print func(*args, **kwargs), dir(func(*args, **kwargs))
+        #funcProfile = cProfile.runctx(func.__code__, self.__dict__, self)
+        funcProfile = None
+        return funcProfile
+
+    return runProfile
+
 def thread(func):
     '''Wrap a function into a python thread'''
     @wraps(func)
@@ -81,21 +154,12 @@ def thread(func):
 
     return runThread
 
-def timeFunction(func):
+def elapsed(func):
     '''Return the time required to run a given function'''
     def run(*args, **kwargs):
         start   = time.time()
         output  = func(*args, **kwargs)
         elapsed = time.time()-start
-        log.debug("timeFunction - %s ran in %fs seconds" % (func, elapsed))
+        log.debug("%s - %f seconds elapsed" % (func, elapsed))
         return output
     return run
-
-if __name__ == '__main__':
-    @thread
-    @timeFunction
-    @catch22
-    def test():
-        time.sleep(3)
-
-    test()
