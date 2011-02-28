b3b967c60a4132bec259f04ee0ea835ed3e77beb
Oliver Palmer <oliverpalmer@opalmer.com>
2011-02-28 11:44:50 -0800

    working on a new network interface library for lib.net.includes

--- test.py
+++ test.py
@@ -1,15 +1,59 @@
-import os
-from xml.dom import minidom
+import pprint
+from PyQt4 import QtNetwork
 
-CWD      = os.path.dirname(os.path.abspath(__file__))
-XML      = os.path.join(CWD, "cfg", "logger.xml")
-dom      = minidom.parse(XML)
-settings = dom.getElementsByTagName("Settings")[0]
-globals  = settings.getElementsByTagName("globals")[0]
-levels   = settings.getElementsByTagName("levels")[0]
+class NetworkInterface(object):
+    def __init__(self, interface):
+        self.interface = interface
+        self.addresses = interface.addressEntries()
+        self.name      = str(interface.name())
+        self.humanName = str(interface.humanReadableName())
+        self.mac       = str(interface.hardwareAddress())
+        self.isLocal   = self._isLocal()
+        self.isValid   = self._isValid()
 
-for attr in globals.getElementsByTagName("attr"):
-    print attr
+    def _isLocal(self):
+        '''Return true of the interface is for the localhost'''
+        return True
 
-for level in levels.getElementsByTagName("level"):
-    print level
+    def _isValid(self):
+        '''
+        Evalulate the interface object and return true if the interface contains
+        valid information and a non-local address.
+        '''
+        if not self.interface.isValid():
+            return False
+
+        #TODO: Add check for non-local address
+        #TODO: Add check for AT LEAST ipv4 addresses
+
+        return True
+
+def interfaces():
+    '''Returns a list of interface objects'''
+    for interface in QtNetwork.QNetworkInterface.allInterfaces():
+        if interface.isValid():
+            yield NetworkInterface(interface)
+
+for interface in interfaces():
+    print interface.humanName
+
+data = {}
+for interface in QtNetwork.QNetworkInterface.allInterfaces():
+    entry = data[str(interface.humanReadableName())] = {}
+    entry['name']      = str(interface.name())
+    entry['humanName'] = str(interface.humanReadableName())
+    entry['mac']       = str(interface.hardwareAddress())
+    entry['valid']     = str(interface.isValid())
+
+    addresses = entry['addresses'] = {}
+    for address in interface.addressEntries():
+        if address.ip().protocol():  # IPv6
+            entry = addresses['IPv6'] = {}
+        else:                        # IPv4
+            entry = addresses['IPv4'] = {}
+        entry['broadcast'] = str(address.broadcast().toString())
+        entry['ip']        = str(address.ip().toString())
+        entry['netmask']   = str(address.netmask().toString())
+        entry['prefix']    = str(address.prefixLength())
+
+#pprint.pprint(data)
