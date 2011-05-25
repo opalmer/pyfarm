6ddd9e06d9309a318d0b779dafe17985bcb2928d
Oliver Palmer <oliverpalmer@opalmer.com>
2011-05-25 10:07:47 -0700

    standalone client/server

--- trunk/lib/net/tcp/rdp/client.py
+++ trunk/lib/net/tcp/rdp/client.py
@@ -21,65 +21,22 @@
 
 import sys
 
-from PyQt4 import QtGui, QtNetwork, QtCore
+from PyQt4 import QtNetwork, QtCore
 from PyQt4.QtCore import Qt
 
 MAC = "qt_mac_set_native_menubar" in dir()
 
 PORT = 9407
-SIZEOF_UINT16 = 2
+UINT16 = 2
 STREAM_VERSION = QtCore.QDataStream.Qt_4_2
 
-class Client(QtGui.QWidget):
-
+class Client(QtCore.QObject):
     def __init__(self, parent=None):
         super(Client, self).__init__(parent)
 
         self.socket = QtNetwork.QTcpSocket()
         self.nextBlockSize = 0
         self.request = None
-
-        roomLabel = QtGui.QLabel("&Room")
-        self.roomEdit = QtGui.QLineEdit()
-        roomLabel.setBuddy(self.roomEdit)
-        regex = QtCore.QRegExp(r"[0-9](?:0[1-9]|[12][0-9]|3[0-4])")
-        self.roomEdit.setValidator(QtGui.QRegExpValidator(regex, self))
-        self.roomEdit.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
-        dateLabel = QtGui.QLabel("&Date")
-        self.dateEdit = QtGui.QDateEdit()
-        dateLabel.setBuddy(self.dateEdit)
-        self.dateEdit.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
-        self.dateEdit.setDate(QtCore.QDate.currentDate().addDays(1))
-        self.dateEdit.setDisplayFormat("yyyy-MM-dd")
-        responseLabel = QtGui.QLabel("Response")
-        self.responseLabel = QtGui.QLabel()
-        self.responseLabel.setFrameStyle(QtGui.QFrame.StyledPanel|
-                                         QtGui.QFrame.Sunken)
-
-        self.bookButton = QtGui.QPushButton("&Book")
-        self.bookButton.setEnabled(False)
-        self.unBookButton = QtGui.QPushButton("&Unbook")
-        self.unBookButton.setEnabled(False)
-        quitButton = QtGui.QPushButton("&Quit")
-
-        if not MAC:
-            self.bookButton.setFocusPolicy(Qt.NoFocus)
-            self.unBookButton.setFocusPolicy(Qt.NoFocus)
-
-        buttonLayout = QtGui.QHBoxLayout()
-        buttonLayout.addWidget(self.bookButton)
-        buttonLayout.addWidget(self.unBookButton)
-        buttonLayout.addStretch()
-        buttonLayout.addWidget(quitButton)
-        layout = QtGui.QGridLayout()
-        layout.addWidget(roomLabel, 0, 0)
-        layout.addWidget(self.roomEdit, 0, 1)
-        layout.addWidget(dateLabel, 0, 2)
-        layout.addWidget(self.dateEdit, 0, 3)
-        layout.addWidget(responseLabel, 1, 0)
-        layout.addWidget(self.responseLabel, 1, 1, 1, 3)
-        layout.addLayout(buttonLayout, 2, 1, 1, 4)
-        self.setLayout(layout)
 
         self.connect(self.socket, QtCore.SIGNAL("connected()"),
                      self.sendRequest)
@@ -90,62 +47,27 @@ class Client(QtGui.QWidget):
         self.connect(self.socket,
                      QtCore.SIGNAL("error(QAbstractSocket::SocketError)"),
                      self.serverHasError)
-        self.connect(self.roomEdit, QtCore.SIGNAL("textEdited(QString)"),
-                     self.updateUi)
-        self.connect(self.dateEdit, QtCore.SIGNAL("dateChanged(QDate)"),
-                     self.updateUi)
-        self.connect(self.bookButton, QtCore.SIGNAL("clicked()"),
-                     self.book)
-        self.connect(self.unBookButton, QtCore.SIGNAL("clicked()"),
-                     self.unBook)
-        self.connect(quitButton, QtCore.SIGNAL("clicked()"), self.close)
-
-        self.setWindowTitle("Building Services")
-
-
-    def updateUi(self):
-        enabled = False
-        if not self.roomEdit.text().isEmpty() and \
-           self.dateEdit.date() > QtCore.QDate.currentDate():
-            enabled = True
-        if self.request is not None:
-            enabled = False
-        self.bookButton.setEnabled(enabled)
-        self.unBookButton.setEnabled(enabled)
-
 
     def closeEvent(self, event):
         self.socket.close()
         event.accept()
 
-
-    def book(self):
-        self.issueRequest(QtCore.QString("BOOK"), self.roomEdit.text(),
-                          self.dateEdit.date())
-
-
-    def unBook(self):
-        self.issueRequest(QtCore.QString("UNBOOK"), self.roomEdit.text(),
-                          self.dateEdit.date())
-
-
-    def issueRequest(self, action, room, date):
+    def issueRequest(self, header, data):
         self.request = QtCore.QByteArray()
         stream = QtCore.QDataStream(self.request, QtCore.QIODevice.WriteOnly)
         stream.setVersion(STREAM_VERSION)
         stream.writeUInt16(0)
-        stream << action << room << date
+        stream << QtCore.QString(header) << QtCore.QString(data)
         stream.device().seek(0)
-        stream.writeUInt16(self.request.size() - SIZEOF_UINT16)
-        self.updateUi()
+        stream.writeUInt16(self.request.size() - UINT16)
         if self.socket.isOpen():
             self.socket.close()
-        self.responseLabel.setText("Connecting to server...")
+        print "Connecting to server..."
         self.socket.connectToHost("localhost", PORT)
 
 
     def sendRequest(self):
-        self.responseLabel.setText("Sending request...")
+        print "Sending request"
         self.nextBlockSize = 0
         self.socket.write(self.request)
         self.request = None
@@ -157,45 +79,32 @@ class Client(QtGui.QWidget):
 
         while True:
             if self.nextBlockSize == 0:
-                if self.socket.bytesAvailable() < SIZEOF_UINT16:
+                if self.socket.bytesAvailable() < UINT16:
                     break
                 self.nextBlockSize = stream.readUInt16()
             if self.socket.bytesAvailable() < self.nextBlockSize:
                 break
-            action = QtCore.QString()
-            room = QtCore.QString()
-            date = QtCore.QDate()
-            stream >> action >> room
-            if action != "ERROR":
-                stream >> date
-            if action == "ERROR":
-                msg = QtCore.QString("Error: %1").arg(room)
-            elif action == "BOOK":
-                msg = QtCore.QString("Booked room %1 for %2").arg(room) \
-                              .arg(date.toString(Qt.ISODate))
-            elif action == "UNBOOK":
-                msg = QtCore.QString("Unbooked room %1 for %2").arg(room) \
-                              .arg(date.toString(Qt.ISODate))
 
-            self.responseLabel.setText(msg)
-            self.updateUi()
+            header = QtCore.QString()
+            data = QtCore.QString()
+            date = QtCore.QDate()
+            stream >> header >> data
+            print "Header Reply:",header
+            print "Data Reply:",data
             self.nextBlockSize = 0
 
 
     def serverHasStopped(self):
-        self.responseLabel.setText(
-                "Error: Connection closed by server")
+        print "Connection closed by server"
         self.socket.close()
-
 
     def serverHasError(self, error):
-        self.responseLabel.setText(QString("Error: %1") \
-                .arg(self.socket.errorString()))
+        print "Error: %s" % self.socket.errorString()
         self.socket.close()
 
-
-app = QtGui.QApplication(sys.argv)
-form = Client()
-form.show()
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+app = QtCore.QCoreApplication(sys.argv)
+client = Client()
+client.issueRequest("TEST", "hello world")
 app.exec_()
-
--- trunk/lib/net/tcp/rdp/server.py
+++ trunk/lib/net/tcp/rdp/server.py
@@ -18,40 +18,24 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
 
-import bisect
-import collections
 import sys
-from PyQt4 import QtGui, QtNetwork, QtCore
+from PyQt4 import QtNetwork, QtCore
 from PyQt4.QtCore import Qt
 
 PORT = 9407
-SIZEOF_UINT16 = 2
-MAX_BOOKINGS_PER_DAY = 5
+UINT16 = 2
 STREAM_VERSION = QtCore.QDataStream.Qt_4_2
-
-# Key = date, value = list of room IDs
-Bookings = collections.defaultdict(list)
-
-
-def printBookings():
-    for key in sorted(Bookings):
-        print key, Bookings[key]
-    print
 
 
 class Thread(QtCore.QThread):
-
-    lock = QtCore.QReadWriteLock()
-
-    def __init__(self, socketId, parent):
+    def __init__(self, socketIdent, parent):
         super(Thread, self).__init__(parent)
-        self.socketId = socketId
-
+        self.socketIdent = socketIdent
 
     def run(self):
         socket = QtNetwork.QTcpSocket()
 
-        if not socket.setSocketDescriptor(self.socketId):
+        if not socket.setSocketDescriptor(self.socketIdent):
             self.emit(QtCore.SIGNAL("error(int)"), socket.error())
             return
 
@@ -60,7 +44,7 @@ class Thread(QtCore.QThread):
             stream = QtCore.QDataStream(socket)
             stream.setVersion(STREAM_VERSION)
 
-            if socket.waitForReadyRead() and socket.bytesAvailable() >= SIZEOF_UINT16:
+            if socket.waitForReadyRead() and socket.bytesAvailable() >= UINT16:
                 nextBlockSize = stream.readUInt16()
 
             else:
@@ -72,159 +56,66 @@ class Thread(QtCore.QThread):
                     self.sendError(socket, "Cannot read client data")
                     return
 
-            action = QtCore.QString()
-            room = QtCore.QString()
-            date = QtCore.QDate()
-            stream >> action
+            header = QtCore.QString()
+            data = QtCore.QString()
+            stream >> header >> data
 
-            if action in ("BOOK", "UNBOOK"):
-                stream >> room >> date
-                try:
-                    Thread.lock.lockForRead()
-                    bookings = Bookings.get(date.toPyDate())
-                finally:
-                    Thread.lock.unlock()
-                uroom = unicode(room)
-            if action == "BOOK":
-                newlist = False
-
-                try:
-                    Thread.lock.lockForRead()
-                    if bookings is None:
-                        newlist = True
-                finally:
-                    Thread.lock.unlock()
-
-                if newlist:
-                    try:
-                        Thread.lock.lockForWrite()
-                        bookings = Bookings[date.toPyDate()]
-                    finally:
-                        Thread.lock.unlock()
-
-                error = None
-                insert = False
-
-                try:
-                    Thread.lock.lockForRead()
-                    if len(bookings) < MAX_BOOKINGS_PER_DAY:
-                        if uroom in bookings:
-                            error = "Cannot accept duplicate booking"
-                        else:
-                            insert = True
-                    else:
-                        error = QString("%1 is fully booked").arg(
-                                        date.toString(Qt.ISODate))
-                finally:
-                    Thread.lock.unlock()
-                if insert:
-                    try:
-                        Thread.lock.lockForWrite()
-                        bisect.insort(bookings, uroom)
-
-                    finally:
-                        Thread.lock.unlock()
-
-                    self.sendReply(socket, action, room, date)
-                else:
-                    self.sendError(socket, error)
-            elif action == "UNBOOK":
-                error = None
-                remove = False
-                try:
-                    Thread.lock.lockForRead()
-                    if bookings is None or uroom not in bookings:
-                        error = "Cannot unbook nonexistent booking"
-                    else:
-                        remove = True
-                finally:
-                    Thread.lock.unlock()
-                if remove:
-                    try:
-                        Thread.lock.lockForWrite()
-                        bookings.remove(uroom)
-                    finally:
-                        Thread.lock.unlock()
-                    self.sendReply(socket, action, room, date)
-                else:
-                    self.sendError(socket, error)
-            else:
-                self.sendError(socket, "Unrecognized request")
+            print "Incoming Header:",header
+            print "Incoming Data:",data
+            self.sendReply(socket, header, data)
             socket.waitForDisconnected()
-            try:
-                Thread.lock.lockForRead()
-                printBookings()
-            finally:
-                Thread.lock.unlock()
-
 
     def sendError(self, socket, msg):
+        print "Sending error"
         reply = QtCore.QByteArray()
         stream = QtCore.QDataStream(reply, QtCore.QIODevice.WriteOnly)
         stream.setVersion(STREAM_VERSION)
         stream.writeUInt16(0)
         stream << QtCore.QString("ERROR") << QtCore.QString(msg)
         stream.device().seek(0)
-        stream.writeUInt16(reply.size() - SIZEOF_UINT16)
+        stream.writeUInt16(reply.size() - UINT16)
         socket.write(reply)
+        print "error sent"
 
 
-    def sendReply(self, socket, action, room, date):
+    def sendReply(self, socket, header, data):
+        print "Sending reply"
         reply = QtCore.QByteArray()
         stream = QtCore.QDataStream(reply, QtCore.QIODevice.WriteOnly)
         stream.setVersion(STREAM_VERSION)
         stream.writeUInt16(0)
-        stream << action << room << date
+        stream << header << data
         stream.device().seek(0)
-        stream.writeUInt16(reply.size() - SIZEOF_UINT16)
+        stream.writeUInt16(reply.size() - UINT16)
         socket.write(reply)
+        print "reply sent"
 
 
 class TcpServer(QtNetwork.QTcpServer):
-
     def __init__(self, parent=None):
         super(TcpServer, self).__init__(parent)
 
 
-    def incomingConnection(self, socketId):
-        thread = Thread(socketId, self)
+    def incomingConnection(self, socketIdent):
+        thread = Thread(socketIdent, self)
         self.connect(thread, QtCore.SIGNAL("finished()"),
                      thread, QtCore.SLOT("deleteLater()"))
         thread.start()
 
 
-class BuildingServicesDlg(QtCore.QObject):
+class Main(QtCore.QObject):
 
     def __init__(self, parent=None):
-        super(BuildingServicesDlg, self).__init__(parent)
-        self.loadBookings()
+        super(Main, self).__init__(parent)
         self.tcpServer = TcpServer(self)
 
         if not self.tcpServer.listen(QtNetwork.QHostAddress("0.0.0.0"), PORT):
-            print "Building Services Server: %s" % self.tcpServer.errorString()
+            print "Error: %s" % self.tcpServer.errorString()
             self.close()
             return
-
-    def loadBookings(self):
-        # Generate fake data
-        import random
-
-        today = QtCore.QDate.currentDate()
-        for i in range(10):
-            date = today.addDays(random.randint(7, 60))
-            for j in range(random.randint(1, MAX_BOOKINGS_PER_DAY)):
-                # Rooms are 001..534 excl. 100, 200, ..., 500
-                floor = random.randint(0, 5)
-                room = random.randint(1, 34)
-                bookings = Bookings[date.toPyDate()]
-                if len(bookings) >= MAX_BOOKINGS_PER_DAY:
-                    continue
-                bisect.insort(bookings, u"%1d%02d" % (floor, room))
-        printBookings()
 
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 app = QtCore.QCoreApplication(sys.argv)
-server = BuildingServicesDlg()
+server = Main()
 app.exec_()
-
