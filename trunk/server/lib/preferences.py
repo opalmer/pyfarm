26f568604b0dfe4ae7011c698794fb457f658718
Oliver Palmer <oliverpalmer@opalmer.com>
2011-12-24 11:24:16 -0800

    inital server preferences

---
+++ trunk/server/lib/preferences.py
@@ -0,0 +1,49 @@
+# No shebang line, this module is meant to be imported
+#
+# This file is part of PyFarm.
+# Copyright (C) 2008-2011 Oliver Palmer
+#
+# PyFarm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyFarm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import ConfigParser
+
+ETC = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "etc"))
+CONFIG_NAME = "server.ini"
+CONFIG = os.path.join(ETC, CONFIG_NAME)
+
+# ensure the preference file exists
+if not os.path.isfile(CONFIG):
+    raise IOError("missing server configuration %s" % CONFIG)
+
+# read configuration
+cfg = ConfigParser.ConfigParser()
+cfg.read(CONFIG)
+
+# establish global preferences
+PORT = cfg.getint('NETWORK', 'port')
+
+# delete temp variables
+del cfg
+del os, ConfigParser
+
+if __name__ == '__main__':
+    import pprint
+
+    local = {}
+    for key, value in locals().items():
+        if key.isupper():
+            local[key] = value
+
+    pprint.pprint(local)
