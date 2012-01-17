279e11a2994fcefc9a2546e6e2b2efe97c7472dd
Oliver Palmer <oliverpalmer@opalmer.com>
2012-01-16 19:12:09 -0800

    removing unused files

--- dev/tools/depgraph/makeDepGraphs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-client=$HOME/pyfarm/Client.py
-main=$HOME/pyfarm/Main.pyw
-
-python py2depgraph.py $client | python depgraph2dot.py | dot -T $1 -o 
-output/Client.$1
-python py2depgraph.py $main | python depgraph2dot.py | dot -T $1 -o 
-output/Main.$1
