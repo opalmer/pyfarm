d356e79b25eb4e5d2e678c98ed7652cb5c279609
Oliver Palmer <oliverpalmer@opalmer.com>
2012-02-21 23:55:52 -0800

    updated tables to not allow nullable values in some fields

--- trunk/common/db/tables.py
+++ trunk/common/db/tables.py
@@ -55,10 +55,10 @@ hosts = sql.Table('pyfarm_hosts', metadata,
     sql.Column('swap_total', sql.Integer),
     sql.Column('swap_usage', sql.Integer),
     sql.Column('cpu_count', sql.Integer),
-    sql.Column('online', sql.Boolean),
+    sql.Column('online', sql.Boolean, nullable=False, default=True),
     sql.Column('groups', sql.String(128), default='*'),
     sql.Column('software', sql.String(256), default="*"),
-    sql.Column('hold', sql.Boolean, default=False),
+    sql.Column('hold', sql.Boolean, nullable=False, default=False),
     sql.Column('frames', sql.String(128), default="")
 )
 
