98331b801d2ed0c7f4dfba70cd3a3580232f40ab
Oliver Palmer <oliverpalmer@opalmer.com>
2012-03-15 01:30:40 -0700

    adding frames module to package

--- trunk/common/db/query/__init__.py
+++ trunk/common/db/query/__init__.py
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
 
-import hosts, jobs
+import hosts, jobs, frames
