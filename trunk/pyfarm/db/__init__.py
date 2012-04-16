2484cecfadbf7f398f8bb0f591f72261695c35c1
Oliver Palmer <oliverpalmer@opalmer.com>
2012-04-15 19:23:55 -0700

    fixed import

--- trunk/pyfarm/db/__init__.py
+++ trunk/pyfarm/db/__init__.py
@@ -16,3 +16,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
 
+from transaction import Transaction
