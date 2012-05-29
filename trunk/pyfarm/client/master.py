246bdd5aa2157b3c3c3c6bd68e4e63f4a0c6d9de
Oliver Palmer <oliverpalmer@opalmer.com>
2012-05-28 21:26:26 -0700

    initial set/retrieve master work, will need clients to auto-insert their host data on launch before finishing this

--- trunk/pyfarm/client/master.py
+++ trunk/pyfarm/client/master.py
@@ -18,36 +18,66 @@
 
 '''module for dealing with storage and retrieval of the master'''
 
+import socket
+from twisted.python import log
+
 from pyfarm import prefs
 
-PORT = prefs.get('network.ports.client')
+HOSTNAME = socket.gethostname()
 
 def getmaster(options):
     '''primary function for retrieving the master'''
-    # ensure both --master and --set-master are not being provided
-    if options.master is not None and options.set_master is not None:
-        raise ValueError("--set-master and --master cannot both be defined")
+    count = 0
+    if prefs.get('client.lookup-master'):
+        options.lookup_master = True
+
+    else:
+        for value in (options.master, options.set_master, options.lookup_master):
+            if value is not None:
+                count += 1
+
+    # ensure no more than a single master flag is used
+    if count > 1:
+        raise ValueError("only one flag can be used to set the master")
+
+    elif options.master:
+        log.msg("master '%s' provided by --master" % options.master)
+        return options.master
+
+    elif options.set_master:
+        log.msg("master '%s' provided by --set-master" % options.set_master)
+        setmaster(options.set_master)
+        return options.set_master
+
+    elif options.lookup_master:
+        return retrievemaster()
+
+    # if no flags have been given determine if we should
+    # use the local preferences or lookup the master in the database
+    elif not count:
+        msg = "no command line flags or preferences overrides found, checking"
+        msg += " master preference"
+        log.msg(msg)
+
+        master = prefs.get('client.master')
+
+        # checks to see if the master has been set, if not then we need
+        # to throw an error
+        if not master:
+            msg = "Master not set in preferences and no other methods of"
+            msg += " master retrieval have been specified.  Please see the "
+            msg += "command line flags or the 'lookup-master' preference."
+            raise ValueError(msg)
+
+        # if the master HAS been set then set the locally before returning
+        # the results
+        return master
 # end getmaster
 
+def setmaster(master):
+    log.msg("setting master to '%s' in database" % master, level="NOT_IMPLEMENTED")
+# end setmaster
 
-#
-## if either master or set_master are set then we should
-## setup the local MASTER variable before we continue
-#if options.master or options.set_master:
-#    port = prefs.get('network.ports.server')
-#    master = options.master or options.set_master
-#    MASTER = (master, port)
-#
-#if options.set_master:
-#    log.msg(
-#        "--set-master database calls not implemented",
-#        level="NOT_IMPLEMENTED",
-#        system="Client"
-#    )
-#
-#elif options.set_master is None and options.master is None:
-#    log.msg(
-#        "master from database not implemented",
-#        level="NOT_IMPLEMENTED",
-#        system="Client"
-#    )
+def retrievemaster():
+    log.msg("retrieving master for '%s'" % HOSTNAME, level="NOT_IMPLEMENTED")
+# end retrievemaster
