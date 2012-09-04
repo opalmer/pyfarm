a07ab25bec8080ce0277ecfa7a067fb9c446ea0f
Oliver Palmer <oliverpalmer@opalmer.com>
2012-09-03 23:46:43 -0700

    - cleaned up unused methods - failing to resolve hostname now results in an error

--- trunk/pyfarm/client/master.py
+++ trunk/pyfarm/client/master.py
@@ -18,66 +18,43 @@
 
 '''module for dealing with storage and retrieval of the master'''
 
-import socket
 from twisted.python import log
 
-from pyfarm import prefs
+import socket
+import logging
 
-HOSTNAME = socket.gethostname()
+from pyfarm.db import query
+from pyfarm import datatypes
 
-def getmaster(options):
-    '''primary function for retrieving the master'''
-    count = 0
-    if prefs.get('client.lookup-master'):
-        options.lookup_master = True
+from twisted.python import log
 
-    else:
-        for value in (options.master, options.set_master, options.lookup_master):
-            if value is not None:
-                count += 1
+HOSTNAME = datatypes.Localhost.net.HOSTNAME
 
-    # ensure no more than a single master flag is used
-    if count > 1:
-        raise ValueError("only one flag can be used to set the master")
+def get(master=None):
+    '''
+    :param string master:
+        if provided then use this value as the master regardless of what
+        the master is in the database or what the hostname is
+    '''
+    if master is None:
+        master = query.hosts.master(HOSTNAME)
 
-    elif options.master:
-        log.msg("master '%s' provided by --master" % options.master)
-        return options.master
+    if master is None:
+        raise ValueError(
+            "expected master to be a non-null value please check your input arguments to pyfarm_client"
+        )
 
-    elif options.set_master:
-        log.msg("master '%s' provided by --set-master" % options.set_master)
-        setmaster(options.set_master)
-        return options.set_master
+    # if we cannot resolve the provided master then we will probably
+    # have trouble connecting so just fail here
+    try:
+        address = socket.gethostbyname(master)
 
-    elif options.lookup_master:
-        return retrievemaster()
+    except socket.gaierror:
+        log.msg(
+            "master '%s' failed to resolve to a valid address" % master,
+            level=logging.ERROR
+        )
+        raise
 
-    # if no flags have been given determine if we should
-    # use the local preferences or lookup the master in the database
-    elif not count:
-        msg = "no command line flags or preferences overrides found, checking"
-        msg += " master preference"
-        log.msg(msg)
-
-        master = prefs.get('client.master')
-
-        # checks to see if the master has been set, if not then we need
-        # to throw an error
-        if not master:
-            msg = "Master not set in preferences and no other methods of"
-            msg += " master retrieval have been specified.  Please see the "
-            msg += "command line flags or the 'lookup-master' preference."
-            raise ValueError(msg)
-
-        # if the master HAS been set then set the locally before returning
-        # the results
-        return master
-# end getmaster
-
-def setmaster(master):
-    log.msg("setting master to '%s' in database" % master, level="NOT_IMPLEMENTED")
-# end setmaster
-
-def retrievemaster():
-    log.msg("retrieving master for '%s'" % HOSTNAME, level="NOT_IMPLEMENTED")
-# end retrievemaster
+    return master
+# end get
