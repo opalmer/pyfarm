b47da08829446d47d872887aa0873211963b957a
Oliver Palmer <oliverpalmer@opalmer.com>
2012-09-11 00:15:32 -0700

    cleaning up old code

--- trunk/pyfarm/queue.py
+++ trunk/pyfarm/queue.py
@@ -22,49 +22,4 @@ of a new job from the database.  This module may later be expanded on or
 replaced.
 '''
 
-import psutil
-import socket
-import logging
 
-from pyfarm.datatypes.network import HOSTNAME
-
-from twisted.python import log as _log
-
-
-def allocateFrame(hostname=None, cpus=None, ram=None, update=False):
-    '''
-    Retrieves a frame from the database according to the current
-    hardware or provided arguments.  After retrieving the frame
-    we then "allocate" the frame to this host and mark it as running.
-
-    All arguments all refer to the requesting system (where we
-    plan to run the job).
-
-    :param boolean update:
-        if True update the given host in the database
-
-    :rtype tuple:
-        returns the jobid and frame id allocated
-    '''
-    def log(msg, level=logging.DEBUG):
-        _log.msg(msg, level=level, system="queue.allocateFrame")
-    # end log
-
-    hostname = hostname or HOSTNAME
-
-    if cpus is None:
-        cpus = psutil.NUM_CPUS
-
-    if ram is None:
-        ram = (psutil.TOTAL_PHYMEM-psutil.avail_phymem()) / 1024 / 1024
-
-    args = (hostname, ram, cpus)
-    log("preparing to allocate frame for %s (ram: %i, cpus: %i)" % args)
-
-    # retrieve the highest priority job
-    job = jobs.select()
-    frame = frames.select()
-# end allocateFrame
-
-if __name__ == '__main__':
-    allocateFrame()
