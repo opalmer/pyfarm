2fefc840002852ffaa7718daf35c82e8a871a430
Oliver Palmer <oliverpalmer@opalmer.com>
2012-10-07 14:50:21 -0700

    initial version with assignment loop

--- lib/python/pyfarm/master/assign.py
+++ lib/python/pyfarm/master/assign.py
@@ -15,3 +15,32 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
+
+import threading
+import datetime
+
+from pyfarm.logger import Logger
+from pyfarm.preferences import prefs
+from pyfarm.utility import ScheduledRun
+
+class Assignment(ScheduledRun, Logger):
+    '''
+    Assignment class which ensures that two assignment
+    '''
+    LOCK = threading.Lock()
+
+    def __init__(self):
+        Logger.__init__(self, self)
+        ScheduledRun.__init__(self, prefs.get('master.assignment-interval'))
+    # end __init__
+
+    def run(self, force=False):
+        with Assignment.LOCK: # only want one thread at a time to have access
+            if not self.shouldRun(force):
+                self.warning("skipping assignment, lastrun < interval")
+                return
+
+            self.lastrun = datetime.datetime.now()
+            self.info("running assignment")
+    # end run
+# end Assignment
