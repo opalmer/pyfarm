e41eeda4e9b964930501f2f94f48aac2b893a3a0
Oliver Palmer <oliverpalmer@opalmer.com>
2012-10-10 00:41:00 -0700

    update to handle larger data sets, still needs to be tested

--- lib/python/pyfarm/master/assign.py
+++ lib/python/pyfarm/master/assign.py
@@ -17,223 +17,166 @@
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
 
 import time
-import datetime
 import threading
-from itertools import chain
+from itertools import ifilter
 
+from pyfarm.db import contexts
 from pyfarm.logger import Logger
 from pyfarm.preferences import prefs
 from pyfarm.utility import ScheduledRun
-from pyfarm.db import session, tables, contexts
-from pyfarm.datatypes.enums import ACTIVE_JOB_STATES, ACTIVE_FRAME_STATES, State
+from pyfarm.db.tables import hosts as _hosts, frames as _frames, jobs as _jobs
+from pyfarm.datatypes.enums import ACTIVE_HOSTS_FRAME_STATES,\
+    ACTIVE_JOB_STATES, ACTIVE_FRAME_STATES
 
 class Assignment(ScheduledRun, Logger):
-    '''
-    Assignment class which ensures that two assignment
-    '''
     LOCK = threading.Lock()
 
     def __init__(self):
         Logger.__init__(self, self)
         ScheduledRun.__init__(self, prefs.get('master.assignment-interval'))
-        self.__jobstates = ",".join([State.get(i) for i in ACTIVE_JOB_STATES])
-        self.__framestates = ",".join([State.get(i) for i in ACTIVE_FRAME_STATES])
     # end __init__
 
-    def getJobs(self, hosts):
-        '''
-        retrieve a single job which is the highest priority and is marked
-        as active
-        '''
-        results = {}
-        start = time.time()
-        self.debug("searching for jobs")
-
-        with contexts.Session(tables.jobs) as jobs:
-            for host in hosts:
-                cpus = host.cpus
-                ram = host.ram_total - host.ram_usage
-                matching_jobs = jobs.query.filter(
-                    tables.jobs.c.state.in_(ACTIVE_JOB_STATES)
-                ).filter(
-                    tables.jobs.c.cpus <= cpus
-                ).filter(
-                    tables.jobs.c.ram <= ram
-                ).order_by(
-                    tables.jobs.c.priority
-                )
-
-                matching_jobs = matching_jobs[-len(hosts):]
-                args = (len(matching_jobs), host.hostname, len(hosts))
-                self.debug("found %s possible jobs for %s (limit: %s)" % args)
-
-                if not matching_jobs:
-                    continue
-                else:
-                    results[host] = matching_jobs
-
-        self.debug("found jobs in %ss" % (time.time()-start))
-        return results or None
-    # end getJobs
-
-    def getFrames(self, jobdata):
-        start = time.time()
-        self.debug("searching for frames in states (%s)" % self.__framestates)
-        results = {}
-        with contexts.Session(tables.frames) as frames:
-            columns = tables.frames.c
-
-            for host, jobs in jobdata.iteritems():
-                for job in jobs:
-                    valid_frames = frames.query.filter(
-                        columns.jobid == job.id
-                    )
-
-                    if job.requeue_failed and job.requeue_max > 0:
-                        valid_frames = valid_frames.filter(
-                            columns.state.in_((State.FAILED, State.QUEUED))
-                        )
-                    else:
-                        valid_frames = valid_frames.filter(
-                            columns.state == State.QUEUED
-                        )
-
-#                    valid_frames = valid_frames.order_by(
-#                        columns.priority
-#                    )
-
-                    valid_frames = valid_frames[-host.cpus:] or []
-
-                    if valid_frames and host not in results:
-                        results[host] = []
-
-                    results[host].extend(valid_frames)
-
-        args = (len(results.keys()), (time.time()-start))
-        self.debug("found frames for %s hosts in %s" % args)
-        return results or None
-    # end getFrames
-
-    def getHosts(self):
-        start = time.time()
-        with contexts.Session(tables.hosts) as hosts:
-            hosts = hosts.query.filter(
-                tables.hosts.c.online == True
-            ).all()
-
-            # nothing to do here if there are not
-            # any hosts online
-            if hosts is None:
-                return
-
-        # retrieve all jobs running on the online hosts
-        with contexts.Session(tables.jobs) as jobs:
-            running_jobs = jobs.query.filter(
-                tables.jobs.c.id.in_(
-                    (job for job, frame in chain(*( host.jobs for host in hosts )))
-                )
-            ).all()
-
-        # TODO: average/estimate required ram based off of frame + job ram estimate?
-        jobs = dict(((job.id, job) for job in running_jobs))
-        valid_hosts = []
-        for host in hosts:
-            for job in (jobs[job_id] for job_id, frame_id in host.jobs):
-                host.cpus -= job.cpus
-                host.ram_usage += job.ram
-
-                if host.cpus <= 0:
-                    self.debug("host %s does not have enough cpus" % host.hostname)
-                    break
-
-                elif host.ram_total <= host.ram_usage:
-                    self.debug("host %s does not have enough ram" % host.hostname)
-                    break
-
-            else:
-                valid_hosts.append(host)
-
-        results = valid_hosts or []
-        self.debug("got %s hosts in %ss" % ((len(results), time.time()-start)))
-        return results or None
-    # end getHosts
-
-
-    def getFramesOnHosts(self):
-        # TODO: implementation (and rename)?)
-        # for host in onlineHosts:
-        # with Session frames
-        # filter ( columns.hostid == host.id)
-        # .filter( columns.state.in_(RUNNING, ASSIGN)
-        # ^ also retrieves the job id(s)
-
-        self.warning("TODO:")
-        self.warning("-----> should get running frames directly from the frames table")
-
-
-    def getWork(self):
-        '''
-        return a dictionary of hosts and frame ids to assign to assign to
-        each host
-        '''
-        self.getFramesOnHosts()
-        hosts = self.getHosts()
-        if hosts is None:
-            self.error("cannot continue, failed to find any hosts for assignment")
-            return
-
-        # search for valid jobs
-        jobdata = self.getJobs(hosts)
-        job_dict = dict((job.id, job)for job in chain(*jobdata.itervalues()))
-        if jobdata is None:
-            self.error("cannot continue, no jobs found to run")
-            return
-
-        # search for valid frames in those jobs
-        frames = self.getFrames(jobdata)
-        if frames is None:
-            self.error("cannot continue, no frames found to run")
-            return
-
-#        print jobdata.keys()
-        for host, frames in frames.iteritems():
-#            if host.cpus <= 0 or host.:
-#                continue
-
-            for frame in sorted(frames, key=lambda frame: frame.priority, reverse=True):
-                job = job_dict[frame.jobid]
-                print host.hostname, host.cpus
-
-                if host.cpus - job.cpus == 0:
-                    print 'no cpus',host.hostname
-                    break
-#                    printor \
-#                    host.ram_usage + job.ram > host.ram_total:
-#                    break
-
-                print host.hostname, frame
-                host.cpus -= job.cpus
-                host.ram_usage += job.ram
-
-        # TODO: set host on frame to hostid
-        # TODO: mark frame as ASSIGN
-        # TODO: set job as RUNNING if not already set
-
-    # end getWork
-
     def run(self, force=False):
-        with Assignment.LOCK: # only want one thread at a time to have access
+        start = time.time()
+
+        with Assignment.LOCK:
             # check one last time before we attempt to run
             # the assignment
             if not self.shouldRun(force):
                 self.warning("skipping assignment, lastrun < interval")
                 return
 
-            self.info("running assignment")
+            # retrieve all online hosts
+            with contexts.Session(_hosts, close_connections=False) as hosts:
+                online_hosts = hosts.query.filter(
+                    _hosts.c.online == True
+                ).all()
 
-            work = self.getWork()
+                if not online_hosts:
+                    self.error("cannot continue, no hosts found online")
+                    return
 
-            self.debug("finished assignment")
-            self.lastrun = datetime.datetime.now()
-    # end run
+            # retrieve the frame(s) running on these hosts
+            hostids = [host.id for host in online_hosts]
+            with contexts.Session(_frames, close_connections=False) as frames:
+                running_frames = frames.query.filter(
+                    _frames.c.host.in_(hostids)
+                ).filter(
+                    _frames.c.state.in_(ACTIVE_HOSTS_FRAME_STATES)
+                ).all()
+
+            # find the base jobs for all of the frames we found
+            # that are currently running
+            jobids = set((frame.job for frame in running_frames))
+            with contexts.Session(_jobs, close_connections=False) as jobs:
+                running_jobs = jobs.query.filter(_jobs.c.id.in_(jobids)).all()
+
+            args = (len(online_hosts), len(running_frames), len(running_jobs))
+            self.debug("found %s online host(s) running %s frame(s) in %s job(s)" % args)
+
+            # go over each running frame and add it's resource usage
+            # to the host running it
+            running_jobs = dict((job.id, job) for job in running_jobs)
+            online_hosts = dict((host.id, host) for host in online_hosts)
+            for frame in running_frames:
+                job = running_jobs[frame.job]
+                host = online_hosts[frame.host]
+                host.cpus -= job.cpus
+                host.ram_usage += frame.ram if frame.ram > job.ram else job.ram
+
+            # filter out any hosts which do not have enough ram
+            # or cpu space left
+            free_hosts = list(
+                ifilter(
+                    lambda host: host.cpus > 0 and host.ram_usage < host.ram_total,
+                    online_hosts.itervalues()
+            ))
+            self.debug("found %s host with resources left over" % len(free_hosts))
+
+            run_hosts = {}
+            with contexts.Session(_jobs, close_connections=False) as jobs:
+                base_query = jobs.query.filter(
+                    _jobs.c.state.in_(ACTIVE_JOB_STATES)
+                )
+
+                if not base_query.count():
+                    self.error("cannot continue, no active jobs")
+                    return
+
+                # Find at least two jobs which match the host and should
+                # not overflow the resources right off the bat.  We want to
+                # retrieve two jobs here in case there are not enough frames
+                # running on the first job to use all resources
+                for host in free_hosts:
+                    cpus = host.cpus
+                    ram = host.ram_total - host.ram_usage
+                    results = base_query.filter(
+                        _jobs.c.cpus <= cpus
+                    ).filter(
+                        _jobs.c.ram < ram
+                    ).order_by(
+                        _jobs.c.priority
+                    ).order_by(
+                        _jobs.c.cpus
+                    ).order_by(
+                        _jobs.c.ram
+                    )[-2:]
+
+                    if not results:
+                        self.warning("failed to find any jobs to run on %s" % host.hostname)
+                        continue
+
+                    run_hosts[host] = results
+
+            with contexts.Session(_frames, close_connections=False) as frames:
+                for host, jobs in run_hosts.iteritems():
+                    # TODO:
+                    # TODO: verify operation and results on db data
+                    # TODO:
+                    for job in reversed(jobs): # reverse so higher ram/cpus/etc come first
+                        args = (job.id, host.hostname)
+                        self.debug("selecting frame(s) from job %s for %s" % args)
+                        enough_ram = host.ram_usage + job.ram < host.ram_total
+                        enough_cpus = host.cpus - job.cpus > 0
+                        index = 0
+
+                        # construct the query we shold
+                        query = frames.query.filter(
+                            _frames.c.state.in_(ACTIVE_FRAME_STATES)
+                        ).order_by(
+                            _frames.c.priority
+                        ).order_by(
+                            _frames.c.attempts
+                        )
+
+                        while enough_ram and enough_cpus:
+                            try:
+                                frame = query[index]
+                                index += 1
+
+                            except IndexError:
+                                break
+
+
+                            # TODO:-------------------------------------------------
+                            #  1. select frames from job
+                            #  2. subtract job resources host (code below)
+                            #  3. if resources left and frames exhausted
+                            #     repeat 1. using secondary job
+                            #  4. break
+                            enough_ram = host.ram_usage + job.ram < host.ram_total
+                            enough_cpus = host.cpus - job.cpus > 0
+                            host.ram_usage += job.ram
+                            host.cpus -= job.cpus
+
+            # TODO:-------------------------------------------------
+            # 1. mark each job being assigned as RUNNING (if changed)
+            # 2. mark each frame as ASSIGN and tag it with the host
+            # 3. send frame ids to host
+
+        self.debug("elapsed for assignment %ss" % (time.time()-start))
 # end Assignment
+
+if __name__ == '__main__':
+    assign = Assignment()
+    assign.run()
