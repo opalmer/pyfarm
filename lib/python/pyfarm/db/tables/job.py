d5f4e3ed067252c87180a8a6e30962b1664cae65
Oliver Palmer <oliverpalmer@opalmer.com>
2012-10-15 23:29:30 -0700

    adding additional columns

--- lib/python/pyfarm/db/tables/job.py
+++ lib/python/pyfarm/db/tables/job.py
@@ -16,9 +16,14 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with PyFarm.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import getpass
+from datetime import datetime
+
 from sqlalchemy import Column, ForeignKey, and_
 from sqlalchemy.orm import relationship
-from sqlalchemy.types import Integer, Boolean
+from sqlalchemy.types import Integer, Boolean, DateTime, Text, \
+    PickleType, String
 
 from pyfarm.db.tables import Base, Frame
 from pyfarm.preferences import prefs
@@ -30,7 +35,7 @@ REQUEUE_FAILED = prefs.get('jobtypes.defaults.requeue-failed')
 
 class Dependency(Base):
     '''
-    Defines a dependency
+    Defines a dependency between a parent job and a child
     '''
     __tablename__ = "pyfarm_job_dependency"
     repr_attrs = ("parent", "dependency")
@@ -54,11 +59,32 @@ class Dependency(Base):
 class Job(Base):
     '''base job definition'''
     __tablename__ = "pyfarm_jobs"
-    repr_attrs = ("id", "state")
+    repr_attrs = (
+        "id", "state", "start_frame", "end_frame", "by_frame", "priority"
+    )
 
+    # frame information
+    start_frame = Column(Integer, nullable=False)
+    end_frame = Column(Integer, nullable=False)
+    by_frame = Column(Integer, default=1)
+    batch_frame = Column(Integer, default=1)
+
+    # state, requeue, and priority
     state = Column(Integer, default=State.QUEUED)
+    priority = Column(Integer, default=prefs.get('jobsystem.priority-default'))
     requeue_failed = Column(Boolean, default=REQUEUE_FAILED)
     requeue_max = Column(Integer, default=REQUEUE_MAX)
+    time_submitted = Column(DateTime, default=datetime.now)
+
+    # job related information
+    cmd = Column(Text(convert_unicode=True), nullable=False)
+    args = Column(PickleType, nullable=False)
+    notes = Column(Text(convert_unicode=True), default="N/A")
+    environ = Column(PickleType, default=os.environ.copy)
+    data = Column(PickleType, default=dict)
+    user = Column(String(256), default=getpass.getuser)
+    ram = Column(Integer, default=None)
+    cpus = Column(Integer, default=None)
 
     # relationship definitions
     frames = relationship('Frame', uselist=True, backref="ref_frames")
@@ -73,11 +99,44 @@ class Job(Base):
                     '(Frame.state == %s)' % State.FAILED
     )
 
-    def __init__(self, state=None):
+    # TODO: attributes for new columns above
+    def __init__(self, cmd, args, start_frame, end_frame, by_frame=None,
+                 batch_frame=None, state=None, priority=None, requeue_max=None,
+                 requeue_failed=None):
+        self.cmd = cmd
+        self.args = args
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+
+        if by_frame is not None:
+            self.by_frame = by_frame
+
+        if self.batch_frame is not None:
+            self.batch_frame = batch_frame
+
+        if priority is not None:
+            self.priority = priority
+
         if state is not None:
             self.state = state
-    # end __init__
 
+        if requeue_failed is None:
+            _requeue_failed = requeue_failed
+
+        else:
+            self.requeue_failed = requeue_failed
+            _requeue_failed = self.requeue_failed
+
+        if requeue_max and not _requeue_failed:
+            raise ValueError(
+                "requeue_max set but requeue_failed evals as None"
+            )
+
+        elif requeue_max is not None:
+            self.requeue_max = requeue_max
+
+
+    # end __init__
 
     @property
     def queued_frames(self):
@@ -109,6 +168,7 @@ class Job(Base):
         if not all_dependencies.count():
             return []
 
+        # TODO: we should probably have the option of checking parent dependencies too
         # iterate over all the dependencies we found and create a list
         # of running dependencies
         query = self.session.query(Job).filter(and_(
--- lib/python/pyfarm/db/tables_prorotype.py
+++ lib/python/pyfarm/db/tables_prorotype.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from pyfarm.db.tables import init, engine, Frame, Host, Group, Software, \
     Job, Dependency
 
-init() # setup tables
+init(True) # setup tables
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -33,26 +33,46 @@ g = Group(hostrow.id, 'group2')
 session.add(g)
 
 # add a job
-j = Job()
+j = Job(1,2)
 session.add(j)
-jobrow = session.query(Job).first()
-
-# add frames
-f = Frame(jobrow.id, 20, state=State.QUEUED, hostid=hostrow.id)
-session.add(f)
-f = Frame(jobrow.id, 21, state=State.ASSIGN, hostid=hostrow.id)
-session.add(f)
-f = Frame(jobrow.id, 22, state=State.RUNNING, hostid=hostrow.id)
-session.add(f)
-f = Frame(jobrow.id, 23, state=State.FAILED, hostid=hostrow.id)
-session.add(f)
+j = Job(1,2,state=State.RUNNING)
+session.add(j)
+j = Job(1,2,state=State.DONE)
+session.add(j)
 
 session.commit()
 
-host = session.query(Host).filter(Host.hostname == hostname).first()
-#print host.running_frames[0].job
+jobs = session.query(Job).all()
+dependency = Dependency(jobs[0], jobs[1])
+session.add(dependency)
+dependency = Dependency(jobs[0], jobs[2])
+session.add(dependency)
+session.commit()
 
-group = session.query(Group).filter(Group.name == 'group1').first()
-#print group.hosts[0].running_frames
+jobs = session.query(Job).all()
 
-print '============', j
+for job in jobs:
+    print job, job.dependencies
+
+
+#
+#
+## add frames
+#f = Frame(jobrow.id, 20, state=State.QUEUED, hostid=hostrow.id)
+#session.add(f)
+#f = Frame(jobrow.id, 21, state=State.ASSIGN, hostid=hostrow.id)
+#session.add(f)
+#f = Frame(jobrow.id, 22, state=State.RUNNING, hostid=hostrow.id)
+#session.add(f)
+#f = Frame(jobrow.id, 23, state=State.FAILED, hostid=hostrow.id)
+#session.add(f)
+
+
+
+#host = session.query(Host).filter(Host.hostname == hostname).first()
+##print host.running_frames[0].job
+#
+#group = session.query(Group).filter(Group.name == 'group1').first()
+##print group.hosts[0].running_frames
+#
+#print '============', j
