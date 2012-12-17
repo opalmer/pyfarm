c94636c0286a21371d3eea47c3537d509e04c12e
Oliver Palmer <oliverpalmer@opalmer.com>
2012-12-16 20:24:13 -0800

    adding imports

--- lib/python/pyfarm/datatypes/__init__.py
+++ lib/python/pyfarm/datatypes/__init__.py
@@ -21,8 +21,4 @@ serves as a central location to store data that is meant
 to be static
 '''
 
-try:
-    from collections import OrderedDict
-
-except ImportError:
-    from ordereddict import OrderedDict
+from pyfarm.datatypes._types import permutations, namedtuple, OrderedDict
