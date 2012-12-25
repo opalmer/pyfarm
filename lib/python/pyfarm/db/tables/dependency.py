00bb6313fef59789c0c2e2dce29f18752c35a0b5
Oliver Palmer <oliverpalmer@opalmer.com>
2012-12-25 13:19:48 -0600

    defined relationship tables

--- lib/python/pyfarm/db/tables/dependency.py
+++ lib/python/pyfarm/db/tables/dependency.py
@@ -23,7 +23,7 @@ dependencies
 
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.types import Integer
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 
 from pyfarm.db.tables import Base
 from pyfarm.db.tables._constants import TABLE_DEPENDENCIES_F2F, \
@@ -32,11 +32,55 @@ from pyfarm.db.tables._constants import TABLE_DEPENDENCIES_F2F, \
 
 class DependencyF2F(Base):
     __tablename__ = TABLE_DEPENDENCIES_F2F
+    repr_attrs = ("_parent", "_child")
+
+    # column definitions
+    _parent = Column(Integer, ForeignKey('%s.id' % TABLE_FRAME))
+    _child = Column(Integer, ForeignKey('%s.id' % TABLE_FRAME))
+
+    # relationship definitions
+    parent = relationship(
+        'Frame',
+        primaryjoin='(Frame.id == DependencyF2F._parent)',
+        backref='ref_dependencyf2f_parent'
+    )
+    child = relationship(
+        'Frame',
+        primaryjoin='(Frame.id == DependencyF2F._child)',
+        backref='ref_dependencyf2f_child'
+    )
+
+    def __init__(self, parent, child):
+        self._parent = parent.id if hasattr(parent, 'id') else parent
+        self._child = child.id if hasattr(child, 'id') else child
+    # end __init__
 # end DependencyF2F
 
 
 class DependencyJ2J(Base):
     __tablename__ = TABLE_DEPENDENCIES_J2J
+    repr_attrs = ("_parent", "_child")
+
+    # column definitions
+    _parent = Column(Integer, ForeignKey('%s.id' % TABLE_JOB))
+    _child = Column(Integer, ForeignKey('%s.id' % TABLE_JOB))
+
+    # relationship definitions
+    parent = relationship(
+        'Frame',
+        primaryjoin='(Frame.id == DependencyJ2J._parent)',
+        backref='ref_dependencyj2j_parent'
+    )
+    child = relationship(
+        'Frame',
+        primaryjoin='(Frame.id == DependencyJ2J._child)',
+        backref='ref_dependencyj2j_child'
+    )
+
+    def __init__(self, parent, child):
+        self._parent = parent.id if hasattr(parent, 'id') else parent
+        self._child = child.id if hasattr(child, 'id') else child
+    # end __init__
 # end DependencyJ2J
 
 if __name__ == '__main__':
