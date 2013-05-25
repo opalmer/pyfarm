4b8544796f3e8ac973e2fad929c56d35184eb1f2
Oliver Palmer <oliverpalmer@opalmer.com>
2013-05-25 02:54:27 -0400

    typo fix

--- dev/travis-ci/build.sh
+++ dev/travis-ci/build.sh
@@ -19,7 +19,7 @@
 title Python Packages
 pip freeze
 
-if [ "BUILD_DOCS" == "true" ]; then
+if [ "$BUILD_DOCS" == "true" ]; then
     title Build Step: documentation
     runcmd make -C docs html
 fi
@@ -27,7 +27,7 @@ fi
 title Build Step: tests
 
 # coveralls does not support 2.5...
-if [ "TRAVIS_PYTHON_VERSION" != "2.5" ]; then
+if [ "$TRAVIS_PYTHON_VERSION" != "2.5" ]; then
     runcmd coverage run --source=pyfarm tests/* --verbose -s
 else
     runcmd nosetests --verbose -s
