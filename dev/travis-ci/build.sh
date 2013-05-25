8ab9d78279f44db0ada0b5ac6740e9f1b92f50fe
Oliver Palmer <oliverpalmer@opalmer.com>
2013-05-24 22:49:59 -0400

    adding headers

--- dev/travis-ci/build.sh
+++ dev/travis-ci/build.sh
@@ -5,7 +5,8 @@
 title Python Packages
 pip freeze
 
-title Building
+title Build Step: documentation
 runcmd make -C docs html
+title Build Step: tests
 runcmd nosetests tests
 todo write out/copy test database configuration file
