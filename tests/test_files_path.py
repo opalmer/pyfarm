acaaea7492d20e19106d1e9fead271a1f04bae95
Oliver Palmer <oliverpalmer@opalmer.com>
2013-05-27 02:47:33 -0400

    adding cleanup and expandenv tests

--- tests/test_files_path.py
+++ tests/test_files_path.py
@@ -18,7 +18,9 @@ import os
 import nose
 import stat
 import uuid
+import shutil
 import tempfile
+from fnmatch import filter as fnfilter
 from nose.tools import raises
 
 from pyfarm.files import path
@@ -28,30 +30,50 @@ CLEAN_ENV = os.environ.copy()
 
 def pretest():
     path.SESSION_DIRECTORY = None
-    os.environ.update(CLEAN_ENV)
+
+    for varname, value in os.environ.copy().iteritems():
+        if varname not in CLEAN_ENV:
+            del os.environ[varname]
+        elif os.environ[varname] != CLEAN_ENV[varname]:
+            os.environ[varname] = CLEAN_ENV[varname]
 
 
-@nose.with_setup(setup=pretest)
+def posttest():
+    # remove any directories we created
+    prefix = path.DEFAULT_DIRECTORY_PREFIX
+    root = os.path.dirname(
+        tempfile.mkdtemp(prefix=prefix)
+    )
+    for dirname in fnfilter(os.listdir(root), "%s*" % prefix):
+        tmpdirectory = os.path.join(root, dirname)
+        shutil.rmtree(tmpdirectory)
+
+
+nosesetup = nose.with_setup(setup=pretest, teardown=posttest)
+mktmp = lambda: tempfile.mkdtemp(prefix=path.DEFAULT_DIRECTORY_PREFIX)
+
+
+@nosesetup
 def test_tempdir_session():
     sessiondir = path.tempdir(unique=False)
     assert path.tempdir(unique=False) == sessiondir == path.SESSION_DIRECTORY
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 def test_tempdir_envvar():
-    os.environ["PYFARM_TMP"] = tempfile.mkdtemp()
+    os.environ["PYFARM_TMP"] = mktmp()
     assert path.tempdir(respect_env=True) == os.environ["PYFARM_TMP"]
     assert path.tempdir(respect_env=False) == path.SESSION_DIRECTORY
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 def test_tempdir_unique():
     assert path.tempdir(
         respect_env=False,
         unique=True) != path.tempdir(respect_env=False, unique=True)
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 def test_tempdir_mode():
     st_mode = os.stat(path.tempdir(unique=True)).st_mode
     assert stat.S_IMODE(st_mode) == path.DEFAULT_PERMISSIONS
@@ -60,7 +82,7 @@ def test_tempdir_mode():
     assert stat.S_IMODE(st_mode) == mymode
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 def test_expandpath():
     os.environ["FOO"] = "foo"
     joined_path = os.path.join("~", "$FOO")
@@ -68,15 +90,40 @@ def test_expandpath():
     assert path.expandpath(joined_path) == expected
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 @raises(EnvironmentError)
 def test_expandenv_raise_enverror():
     path.expandenv(str(uuid.uuid4()))
 
 
-@nose.with_setup(setup=pretest)
+@nosesetup
 @raises(ValueError)
 def test_expandenv_raise_valuerror():
     var = str(uuid.uuid4())
     os.environ[var] = ""
     path.expandenv(var)
+
+
+@nosesetup
+def test_expandenv_path_validation():
+    envvars = {
+        "FOO1": mktmp(), "FOO2": mktmp(),
+        "FOO3": "<unknown_foo>",
+        "FOOBARA": os.pathsep.join(["$FOO1", "$FOO2", "$FOO3"])
+    }
+    os.environ.update(envvars)
+    assert path.expandenv("FOOBARA") == [os.environ["FOO1"], os.environ["FOO2"]]
+
+
+@nosesetup
+def test_expandenv_path_novalidation():
+    envvars = {
+        "FOO4": mktmp(), "FOO5": mktmp(),
+        "FOO6": "<unknown_foo>",
+        "FOOBARB": os.pathsep.join(["$FOO5", "$FOO4", "$FOO6"])
+    }
+    os.environ.update(envvars)
+    expanded = path.expandenv("FOOBARB", validate=False)
+    assert expanded == [
+        os.environ["FOO5"], os.environ["FOO4"], os.environ["FOO6"]
+    ]
