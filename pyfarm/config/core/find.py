78da66b50657f5bde687c76d842adbb2d095aad5
Oliver Palmer <oliverpalmer@opalmer.com>
2013-05-31 01:50:40 -0400

    initial core.find tests

--- pyfarm/config/core/find.py
+++ pyfarm/config/core/find.py
@@ -21,6 +21,7 @@ or files on disk.
 """
 
 import os
+import copy
 from itertools import imap
 from os.path import join, isfile, isdir
 
@@ -36,84 +37,149 @@ DEFAULT_SUBDIR = "etc"
 DEFAULT_USER_PATHS = False
 DEFAULT_SYSTEM_PATHS = False
 DEFAULT_CONFIG_ROOT = join(PYROOT, "config", "etc")
+DEFAULT_VERSION = list(__version__)
 
 
-def configDirectories(
-        roots=None, user=DEFAULT_USER_PATHS, system=DEFAULT_SYSTEM_PATHS):
+def _versionSubdirs(version):
     """
-    generator which emits a list of paths were we should
-    search for configurationfiles
-
-    :param list roots:
-        additional root directories to search for preferences in
-
-    :param bool user:
-        If True then add user data directories as well
-
-    :param bool system:
-        if True then add system data directories as well
-
-    :param bool cull:
-        if True then remove directories which do not contain
-        preference files
+    takes a version list and returns a list of strings which should
+    serve as subdirectory names
     """
-    if roots is None:
-        roots = [DEFAULT_CONFIG_ROOT]
-    elif isinstance(roots, basestring):
-        roots = [roots]
-
-    assert isinstance(__version__, (list, tuple)), \
-        "expected list or tuple for version"
-
-    appdirs = AppDirs("pyfarm", "pyfarm")
-
-    # environment paths before go before our paths
-    if "PYFARM_ETC" in os.environ:
-        roots[:0] = filter(bool, os.environ["PYFARM_ETC"].split(":"))
-
-    # followed by the system
-    if system:
-        roots.insert(0, os.path.join(appdirs.site_data_dir, DEFAULT_SUBDIR))
-
-    # ... then user paths
-    if user:
-        roots.insert(0, os.path.join(appdirs.user_data_dir, DEFAULT_SUBDIR))
-
+    assert isinstance(version, list), "expected a list for `version`"
     # generate a list of subdirectories to visit so
     # preference files can be locked off
     version_subdirs = [""]
     version_parts = []
-    for v in imap(str, __version__):
+
+    for v in imap(str, version):
         version_parts.append(v)
-        add_x_count = len(__version__) - len(version_parts)
+        add_x_count = len(version) - len(version_parts)
         version_entry = ".".join(version_parts + ['x'] * add_x_count)
         version_subdirs.append(version_entry)
 
     # explict version should be first
     version_subdirs.reverse()
 
-    for root, versiondir in product(roots, version_subdirs):
+    return version_subdirs
+
+
+def configDirectories(roots=None, user=None, system=None, version=None,
+                      must_exist=True):
+    """
+    generator which emits a list of paths were we should
+    search for configuration files
+
+    :type roots: str or list
+    :param roots:
+        additional root directories to search for preferences in
+
+    :type user: str or list
+    :param user:
+        The location the user configuration directory exists.  If this
+        value is not provided it will be set
+
+    :type system: str or list
+    :param system:
+        if True then add system data directories as well
+
+    :type version: list
+    :param version:
+        List containing the version broken down into individual components.
+        Some examples of this conversion include:
+
+            * "1.0.0" -> ["1", "0", "0"]
+            * "1.0" -> ["1", "0"]
+
+    :type must_exist: bool
+    :param must_exist:
+        if True then only return directories which exist
+    """
+    appdirs = AppDirs("pyfarm", "pyfarm")
+    version = DEFAULT_VERSION if version is None else version
+    rootdirs = copy.copy(roots)
+
+    if roots is None:
+        rootdirs = [DEFAULT_CONFIG_ROOT]
+    elif isinstance(roots, basestring):
+        rootdirs = [roots]
+
+    # setup user
+    if user is None:
+        user = [os.path.join(appdirs.user_data_dir, DEFAULT_SUBDIR)]
+
+    elif "PYFARM_CFGROOT_USER" in os.environ:
+        user = filter(bool, os.environ["PYFARM_CFGROOT_USER"].split(os.pathsep))
+
+    elif isinstance(user, basestring):
+        user = [user]
+
+    # setup system
+    if system is None:
+        system = [os.path.join(appdirs.site_data_dir, DEFAULT_SUBDIR)]
+
+    elif "PYFARM_CFGROOT_SYSTEM" in os.environ:
+        system = filter(bool, os.environ["PYFARM_CFGROOT_SYSTEM"].split(os.pathsep))
+
+    elif isinstance(system, basestring):
+        system = [system]
+
+    # sanity check before we do anything else
+    assert isinstance(version, list), \
+        "expected list or tuple for `version`"
+    assert isinstance(user, list), \
+        "failed to convert %s for `user` into a list" % user
+    assert isinstance(system, list), \
+        "failed to convert %s for `system` into a list" % system
+    assert isinstance(rootdirs, list), \
+        "failed to convert %s for `roots` into a list" % rootdirs
+
+    # environment paths before go before our paths
+    # NOTE: these are not the same thing as the initial value for `rootdors`
+    if "PYFARM_CFGROOT" in os.environ:
+        rootdirs[:0] = filter(bool, os.environ["PYFARM_CFGROOT"].split(os.pathsep))
+
+    # followed by the system
+    if system:
+        rootdirs[:0] = system
+
+    # ... then user paths
+    if user:
+        rootdirs[:0] = user
+
+    version_subdirs = _versionSubdirs(version)
+
+    paths = []
+    for root, versiondir in product(rootdirs, version_subdirs):
         path = os.path.join(root, versiondir) if versiondir else root
-        if isdir(path):
-            yield path
-# end configDirectories
+        if path not in paths and (not must_exist or isdir(path)):
+            paths.append(path)
+
+    return paths
 
 
-def configFiles(name, user=DEFAULT_USER_PATHS, system=DEFAULT_SYSTEM_PATHS):
+def configFiles(name, roots=None, user=None, system=None, must_exist=True):
     """
     Finds a file by the given name in one of
     the configuration directories.
+
+    :param roots:
+        see `roots` in :py:func:`.configDirectories` for details
+
+    :param user:
+        see `user` in :py:func:`.configDirectories` for details
+
+    :param system:
+        see `system` in :py:func:`.configDirectories` for details
+
+    :param must_exist:
+        see `must_exist` in :py:func:`.configDirectories` for details
     """
-    searched = []
     filepaths = []
 
-    for directory in configDirectories(user=user, system=system):
+    for directory in configDirectories(roots=roots, user=user, system=system):
         filepath = join(directory, name)
-        if isfile(filepath):
+        if filepath not in filepaths and (not must_exist or isfile(filepath)):
             filepaths.append(filepath)
-        else:
-            searched.append(directory)
 
-    filepaths.reverse()  # needs to be loaded in the opposite order
+    # filepaths.reverse()  # needs to be loaded in the opposite order
     return filepaths
-# end configFiles
---
+++ tests/test_config_core_find.py
@@ -0,0 +1,195 @@
+# No shebang line, this module is meant to be imported
+#
+# Copyright 2013 Oliver Palmer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from nose import with_setup
+from nose.tools import raises
+
+from core.prepost import mktmp, pretest_cleanup_env, posttest_cleanup_files
+from pyfarm.config.core import find
+from pyfarm.files.stream import dumpYaml
+
+try:
+    from itertools import product
+except ImportError:
+    from pyfarm.backports import product
+
+VERSION = (1, 2, 3)
+
+setup = with_setup(
+    setup=pretest_cleanup_env, teardown=posttest_cleanup_files
+)
+
+@raises(AssertionError)
+@setup
+def test_directories_version_error():
+    find.configDirectories(version=0)
+
+
+@raises(AssertionError)
+@setup
+def test_directories_user_error():
+    find.configDirectories(user=0)
+
+
+@raises(AssertionError)
+@setup
+def test_directories_system_error():
+    find.configDirectories(system=0)
+
+
+@raises(AssertionError)
+@setup
+def test_directories_roots_error():
+    find.configDirectories(roots=0)
+
+
+@setup
+def test_output_types():
+    assert isinstance(find.configDirectories(), list)
+    assert isinstance(find.configFiles(""), list)
+
+
+@setup
+def test_directories_roots():
+    roots = [mktmp() for _ in xrange(3)]
+    assert roots == find.configDirectories(roots=roots)
+
+
+@setup
+def test_directories_roots_environ():
+    roots = [mktmp() for _ in xrange(3)]
+    os.environ["PYFARM_CFGROOT"] = os.pathsep.join(roots)
+    assert roots == find.configDirectories(roots=roots)
+
+
+@setup
+def test_unversioned_directories():
+    user = [mktmp() for _ in xrange(3)]
+    system = [mktmp() for _ in xrange(3)]
+    roots = [mktmp() for _ in xrange(3)]
+
+    try_kwargs = (
+        {},
+        {"roots": roots},
+        {"roots": roots, "user": user},
+        {"roots": roots, "system": system},
+        {"roots": roots, "user": user, "system": system},
+    )
+    expected_results = (
+        [find.DEFAULT_CONFIG_ROOT],
+        roots,
+        user + roots,
+        system + roots,
+        user + system + roots
+    )
+
+    for kwargs, expected in zip(try_kwargs, expected_results):
+        kwargs.setdefault("version", []) # just in case one was created somewhere
+        assert expected == find.configDirectories(**kwargs)
+
+@setup
+def test_versioned_directories():
+    user = [mktmp() for _ in xrange(3)]
+    system = [mktmp() for _ in xrange(3)]
+    roots = [mktmp() for _ in xrange(3)]
+    version = [1, 2, 3]
+    versions = find._versionSubdirs(version)
+
+    try_kwargs = (
+        {},
+        {"roots": roots},
+        {"roots": roots, "user": user},
+        {"roots": roots, "system": system},
+        {"roots": roots, "user": user, "system": system},
+    )
+    expected_results = (
+        [find.DEFAULT_CONFIG_ROOT],
+        roots,
+        user + roots,
+        system + roots,
+        user + system + roots
+    )
+
+    # because of latency issues on travis-ci and because we don't care
+    # about the directories existing, remove paths we do don't care
+    # about as a filter
+    all_config_dirs = user + system + roots
+    not_a_tmp_path = lambda path: any(True for d in all_config_dirs if path.startswith(d))
+
+    for kwargs, expected in zip(try_kwargs, expected_results):
+        all_paths = []
+        kwargs.setdefault("version", version)
+        kwargs.setdefault("must_exist", False)
+
+        for root, versiondir in product(expected, versions):
+            path = os.path.join(root, versiondir) if versiondir else root
+            if path not in all_paths:
+                all_paths.append(path)
+
+        filtered_config_dirs = filter(
+            not_a_tmp_path,
+            find.configDirectories(**kwargs)
+        )
+
+        if filtered_config_dirs:
+            assert all_paths == filtered_config_dirs
+
+
+@setup
+def test_files_by_name():
+    ymlname = "pyfarm_unittest.yml"
+    user = [mktmp() for _ in xrange(3)]
+    system = [mktmp() for _ in xrange(3)]
+    roots = [mktmp() for _ in xrange(3)]
+
+    try_kwargs = (
+        {"roots": roots},
+        {"roots": roots, "user": user},
+        {"roots": roots, "system": system},
+        {"roots": roots, "user": user, "system": system},
+    )
+    expected_results = (
+        roots,
+        user + roots,
+        system + roots,
+        user + system + roots
+    )
+
+    for kwargs, expected in zip(try_kwargs, expected_results):
+        filenames = []
+
+        for filepath in find.configDirectories(**kwargs):
+            filename = os.path.join(filepath, ymlname)
+            if filename not in filenames:
+                dumpYaml("", filename)
+                if "TRAVIS" in os.environ:
+                    for i in xrange(50):
+                        if os.path.isfile(filename):
+                            break
+                    else:
+                        raise OSError("file not dumped %s" % filename)
+
+                filenames.append(filename)
+
+        yaml_files = find.configFiles(
+            ymlname,
+            roots=kwargs.get("roots"),
+            user=kwargs.get("user"),
+            system=kwargs.get("system")
+        )
+        #
+        assert yaml_files == filenames
