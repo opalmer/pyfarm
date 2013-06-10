33b08243fe5b9d91c4a8cf3707502083fe258ff5
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-09 21:48:29 -0400

    added os field to model

--- pyfarm/admin/db/core.py
+++ pyfarm/admin/db/core.py
@@ -24,24 +24,42 @@ class CoreTableView(ModelView):
     Subclass of the standard :class:`ModelView` which takes into
     account contextual information such as a task or agent's state.
     """
+    form_args = {}
+    form_overrides = {}
+    column_formatters = {}
+
     def __init__(self, model, session=db.session, **kwargs):
         kwargs.setdefault("category", "Tables")
 
         # if the model has a state attribute then we need to create
         # a menu to display the state
         if hasattr(model, "state") and model.STATE_ENUM is not None:
-            self.form_overrides = {"state": SelectField}
+            self.form_overrides.update(state=SelectField)
 
             form_args = []
             for value in sorted(model.STATE_ENUM.values()):
                 key = model.STATE_ENUM.get(value)
                 form_args.append((value, key.capitalize()))
 
-            self.form_args = {"state": {"choices": form_args, "coerce": int}}
-            self.column_formatters = {
-                "state": lambda view, context, model, name:
-                    model.STATE_ENUM.get(model.state).capitalize()
-            }
+            self.form_args.update(state={"choices": form_args, "coerce": int})
+            self.column_formatters.update(state=
+                lambda view, context, model, name:
+                    model.STATE_ENUM.get(model.state).capitalize())
+
+        # if the model has an os (operating system) attribute then
+        # well need the proper column formatter and input handler
+        if hasattr(model, "os") and model.OS_ENUM is not None:
+            self.form_overrides.update(os=SelectField)
+
+            form_args = []
+            for value in sorted(model.OS_ENUM.values()):
+                key = model.OS_ENUM.get(value)
+                form_args.append((value, key.capitalize()))
+
+            self.form_args.update(os={"choices": form_args, "coerce": int})
+            self.column_formatters.update(os=
+                lambda view, context, model, name:
+                    model.OS_ENUM.get(model.os).capitalize())
 
         if "endpoint" in kwargs:
             kwargs["endpoint"] = "table/%s" % kwargs["endpoint"]
