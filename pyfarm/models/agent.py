c753e47a2f325bb4a385a33d73e5ec6abd200a41
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-09 21:49:09 -0400

    added cpus/ran/port/os columns

--- pyfarm/models/agent.py
+++ pyfarm/models/agent.py
@@ -17,21 +17,27 @@
 from sqlalchemy.orm import validates
 
 from pyfarm.flaskapp import db
-from pyfarm.config.enum import HostState
+from pyfarm.config.enum import HostState, OperatingSystem
 from pyfarm.models.mixins import StateMixin, RandIdMixin
 from pyfarm.models.constants import (MAX_HOSTNAME_LENGTH,
-    MAX_IPV4_LENGTH, REGEX_IPV4, REGEX_HOSTNAME, TABLE_AGENT)
+    MAX_IPV4_LENGTH, REGEX_IPV4, REGEX_HOSTNAME, TABLE_AGENT,
+    MIN_RAM_MB, MAX_RAM_MB, MIN_AGENT_PORT, MAX_AGENT_PORT)
 
 
 class Agent(db.Model, RandIdMixin, StateMixin):
     __tablename__ = TABLE_AGENT
     STATE_ENUM = HostState()
+    OS_ENUM = OperatingSystem()
 
     # columns
+    enabled = db.Column(db.Boolean, default=True)
     hostname = db.Column(db.String(MAX_HOSTNAME_LENGTH), nullable=False)
     ip = db.Column(db.String(MAX_IPV4_LENGTH), nullable=False)
+    port = db.Column(db.Integer, nullable=False)
     subnet = db.Column(db.String(MAX_IPV4_LENGTH), nullable=False)
-    enabled = db.Column(db.Boolean, default=True)
+    cpus = db.Column(db.Integer, nullable=False)
+    ram = db.Column(db.Integer, nullable=False)
+    os = db.Column(db.Integer, nullable=False)
 
     # relationships
     tasks = db.relationship("Task", backref="agent", lazy="dynamic")
@@ -47,5 +53,38 @@ class Agent(db.Model, RandIdMixin, StateMixin):
     def validate_address(self, key, value):
         if not REGEX_IPV4.match(value):
             raise ValueError("%s is not valid for %s" % (value, key))
+
+        return value
+
+    @validates("cpus")
+    def validate_cpus(self, key, value):
+        if value < 1:
+            raise ValueError("at least one cpu is required")
+
+        return value
+
+    @validates("ram")
+    def validate_ram(self, key, value):
+        if value < MIN_RAM_MB or value > MAX_RAM_MB:
+            args = (MIN_RAM_MB, MAX_RAM_MB)
+            raise ValueError("ram must be between %s and %s megabytes" % args)
+
+        return value
+
+    @validates("os")
+    def validates_os(self, key, value):
+        if value not in self.OS_ENUM:
+            msg = "`%s` is not a valid operating system, " % value
+            msg += "valid systems are %s" % self.OS_ENUM.values()
+            raise ValueError(msg)
+
+        return value
+
+    @validates("port")
+    def validates_port(self, key, value):
+        if value < MIN_AGENT_PORT or value > MAX_AGENT_PORT:
+            args = (MIN_AGENT_PORT, MAX_AGENT_PORT)
+            msg = "port number must be between %s and %s" % args
+            raise ValueError(msg)
 
         return value
