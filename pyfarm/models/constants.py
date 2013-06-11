5cacb9324eb935812e0d1da20adf68a45de6f323
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-10 22:44:20 -0400

    cleanup (moved into configs)

--- pyfarm/models/constants.py
+++ pyfarm/models/constants.py
@@ -14,18 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-
-REGEX_IPV4 = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
-REGEX_HOSTNAME = re.compile("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$")
-
-MAX_HOSTNAME_LENGTH = 255
-MAX_SOFTWARE_LENGTH = 128
-MAX_GROUP_LENGTH = 128
-MAX_IPV4_LENGTH = 15
-MAX_USERNAME_LENGTH = 255
-
 TABLE_PREFIX = "pyfarm_"
 TABLE_TASK = "%stask" % TABLE_PREFIX
 TABLE_AGENT = "%sagent" % TABLE_PREFIX
 TABLE_AGENT_TAGS = "%s_tags" % TABLE_AGENT
+
