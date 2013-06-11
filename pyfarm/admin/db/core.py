3e58475a7216c1e1e1d7c15836b259a4e2a06ce0
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-10 21:35:14 -0400

    adding proper calls to update(), column enum defaults now supported

--- pyfarm/admin/db/core.py
+++ pyfarm/admin/db/core.py
@@ -24,22 +24,44 @@ class CoreTableModelView(ModelView):
     Subclass of the standard :class:`ModelView` which takes into
     account contextual information such as a task or agent's state.
     """
+    STATE_DEFAULT = None
+
     def __init__(self, model, session=db.session, **kwargs):
         kwargs.setdefault("category", "Database")
+        kwargs.setdefault("endpoint", "tables/%s" % model.__tablename__)
+
+        if self.form_overrides is None:
+            self.form_overrides = {}
+
+        if self.form_args is None:
+            self.form_args = {}
 
         # if the model has a state attribute then we need to create
         # a menu to display the state
         if hasattr(model, "state") and model.STATE_ENUM is not None:
-            self.form_overrides = {"state": SelectField}
+            self.form_overrides.update(state=SelectField)
 
             form_args = []
-            for value in sorted(model.STATE_ENUM.values()):
+            default_index = None
+            for index, value in enumerate(sorted(model.STATE_ENUM.values())):
                 key = model.STATE_ENUM.get(value)
                 form_args.append((value, key.capitalize()))
 
-            self.form_args = {"state": {"choices": form_args, "coerce": int}}
+                # set the default index if it matches STATE_DEFAULT
+                if model.STATE_DEFAULT == value:
+                    default_index = index
 
+            # if a default index was provided then use it to
+            # reorder the state menu
+            if default_index is not None:
+                default_value = model.STATE_ENUM.get(
+                    model.STATE_DEFAULT).capitalize()
+                value = (model.STATE_DEFAULT, default_value)
+                print value
+                index = form_args.index(value)
+                del form_args[index]
+                form_args.insert(0, value)
 
-        kwargs.setdefault("endpoint", "tables/%s" % model.__tablename__)
+            self.form_args.update(state={"choices": form_args, "coerce": int})
 
         super(CoreTableModelView, self).__init__(model, session, **kwargs)
