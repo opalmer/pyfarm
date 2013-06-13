ac59ea465223ac95943ae5697d0a1c933ea7e09d
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-12 22:05:35 -0400

    added floatrange

--- pyfarm/utility.py
+++ pyfarm/utility.py
@@ -23,6 +23,7 @@ import os
 import socket
 import getpass
 import binascii
+import numpy
 
 
 def user():
@@ -52,3 +53,53 @@ def randstr():
 def randint():
     """returns a base 16 integer from :func:`randstr`"""
     return int(randstr(), 16)
+
+
+def floatrange(start, end, by=1, cutoff=True, create_endpoint=True):
+    """
+    Produces an array which can contain floats.  Results are
+    produced by :func:`numpy.arange`
+
+    :type start: int or float
+    :param start:
+        the number to start the range at
+
+    :type end: int or float
+    :param end:
+        the number to finish the range at
+
+    :type by: int or float
+    :param by:
+        the 'step' to use in the range
+
+    :type cutoff: bool
+    :param cutoff:
+        If True then don't produce numbers that exceed `end`
+
+    :type create_endpoint: bool
+    :param create_endpoint:
+        if True then always emit `end` as the last number in the range
+
+    Sample Usage:
+
+        >>> list(floatrange(1, 5))
+        [1, 2, 3, 4, 5]
+
+        >>> list(floatrange(1, 5, .5, cutoff=False, create_endpoint=False))
+        [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5]
+
+        >>> list(floatrange(1, 5, .75, cutoff=True, create_endpoint=True))
+        [1.0, 1.75, 2.5, 3.25, 4.0, 4.75, 5.0]
+    """
+    if end < start:
+        raise ValueError("`end` must be greater than `start`")
+
+    last_value = None
+    for value in numpy.arange(start, end + 1, by):
+        last_value = value
+        if cutoff and value > end:
+            break
+        yield value
+
+    if create_endpoint and last_value != end:
+        yield float(end) if isinstance(by, float) else end
