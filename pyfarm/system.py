7552ddd5860797b13cca8876430780a91b5b0aaf
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-15 16:21:44 -0400

    moved tests into test module

--- pyfarm/system.py
+++ pyfarm/system.py
@@ -29,8 +29,11 @@ from warnings import warn
 
 try:
     import pwd
+    _user = lambda: pwd.getpwuid(os.getuid())[0]
+
 except ImportError:
-    pwd = None
+    import getpass
+    _user = lambda: getpass.getuser()
 
 try:
     import multiprocessing
@@ -44,11 +47,7 @@ import netifaces
 from pyfarm.warning import NotImplementedWarning, NetworkWarning
 from pyfarm.ext.config.core.loader import Loader
 from pyfarm.ext.config.enum import OperatingSystem as _OperatingSystem
-from pyfarm.ext.utility import user, convert
-
-# OperatingSystem = _OperatingSystem()
-# OS = OperatingSystem.get()
-# OSNAME = OperatingSystem.get(OS)
+from pyfarm.utility import convert
 
 
 def user():
@@ -56,25 +55,8 @@ def user():
     Returns the current user name.  On posix based platforms this uses
     :func:`pwd.getpwuid` and on windows it falls back to
     :func:`getpass.getuser`.
-
-    >>> assert isinstance(user(), basestring)
     """
-    if pwd is not None:
-        return pwd.getpwuid(os.getuid())[0]
-    else:
-        return getpass.getuser()
-
-
-class FileSystemInfo(object):
-    """
-    .. note::
-        This class has already been instanced onto `pyfarm.system.filesystem`
-
-    Nanespace class which returns information about the file system
-    including io counters, partitions, and disk usage.
-    """
-    # TODO: property for filesytem case sensitivity
-    # TODO: ------ IO COUNTERS -------
+    return _user()
 
 
 class OperatingSystemInfo(object):
@@ -94,19 +76,13 @@ class OperatingSystemInfo(object):
     # TODO: windows version information (platform?)
     # TODO: Python implementation
     # TODO: Python version
-
-    def todo(self):
-        """
-        >>> operating_system.todo()
-        """
-        raise NotImplementedError("MOVE -ALL- DOCTESTS INTO DEDICATED FILE")
+    def __init__(self):
+        self.enum = _OperatingSystem()
 
     def uptime(self):
         """
         Returns the amount of time the system has been running in
         seconds
-
-        >>> assert time.time() - psutil.BOOT_TIME <= operating_system.uptime()
         """
         return time.time() - psutil.BOOT_TIME
 
@@ -128,10 +104,6 @@ class NetworkInfo(object):
         Utility method which returns True if the given address is 'public'.
         This simply means that we should be able to use the provided address
         to contact or be contacted by other hosts on the network.
-
-        >>> assert not network.isPublic("0.0.0.0")
-        >>> assert not network.isPublic("127.0.0.1")
-        >>> assert not network.isPublic("169.254.0.0")
         """
         address = ipaddress.IPv4Address(
             unicode(address) if isinstance(address, str) else address)
@@ -153,8 +125,6 @@ class NetworkInfo(object):
         """
         Returns the total number of packets sent over the network
         interface provided by :meth:`interface`
-
-        >>> assert network.packetsSent() >= psutil.network_io_counters(pernic=True)[network.interface()].packets_sent
         """
         return self._iocounter.packets_sent
 
@@ -162,8 +132,6 @@ class NetworkInfo(object):
         """
         Returns the total number of packets received over the network
         interface provided by :meth:`interface`
-
-        >>> assert network.packetsReceived() >= psutil.network_io_counters(pernic=True)[network.interface()].packets_recv
         """
         return self._iocounter.packets_recv
 
@@ -171,8 +139,6 @@ class NetworkInfo(object):
         """
         Amount of data sent in megabytes over the network
         interface provided by :meth:`interface`
-
-        >>> assert network.dataSent() >= convert.bytetomb(psutil.network_io_counters(pernic=True)[network.interface()].bytes_sent)
         """
         return convert.bytetomb(self._iocounter.bytes_sent)
 
@@ -180,8 +146,6 @@ class NetworkInfo(object):
         """
         Amount of data received in megabytes over the network
         interface provided by :meth:`interface`
-
-        >>> assert network.dataReceived() >= convert.bytetomb(psutil.network_io_counters(pernic=True)[network.interface()].bytes_recv)
         """
         return convert.bytetomb(self._iocounter.bytes_recv)
 
@@ -189,8 +153,6 @@ class NetworkInfo(object):
         """
         Returns the number of packets which we failed
         to receive on the network interface provided by :meth:`interface`
-
-        >>> assert network.errorCountIncoming() >= convert.bytetomb(psutil.network_io_counters(pernic=True)[network.interface()].errin)
         """
         return self._iocounter.errin
 
@@ -198,17 +160,13 @@ class NetworkInfo(object):
         """
         Returns the number of packets which we failed
         to receive on the network interface provided by :meth:`interface`
-
-        >>> assert network.errorCountOutgoing() >= convert.bytetomb(psutil.network_io_counters(pernic=True)[network.interface()].errout)
         """
         return self._iocounter.errout
 
     def hostname(self, fqdn=True):
         """
-        returns the hostname of this machine
-
-        >>> assert network.hostname() == socket.getfqdn()
-        >>> assert network.hostname(fqdn=False) == socket.gethostname()
+        Returns the hostname of this machine.  If `fqdn` is True then
+        return the fully qualified hostname
         """
         if fqdn:
             return socket.getfqdn()
@@ -216,12 +174,7 @@ class NetworkInfo(object):
             return socket.gethostname()
 
     def addresses(self):
-        """
-        Returns a list of all non-local ip addresses.
-
-        >>> assert len(network.addresses())
-        >>> assert all(network.isPublic(a) for a in network.addresses())
-        """
+        """Returns a list of all non-local ip addresses."""
         output = []
         for interface in self.interfaces():
             addrinfo = netifaces.ifaddresses(interface)
@@ -233,15 +186,7 @@ class NetworkInfo(object):
         return output
 
     def interfaces(self):
-        """
-        returns the names of all valid network interface names
-
-        >>> names = network.interfaces()
-        >>> assert all(name in netifaces.interfaces() for name in names)
-        >>> assert len(netifaces.interfaces()) >= len(network.interfaces())
-        >>> addresses = map(netifaces.ifaddresses, names)
-        >>> assert all(socket.AF_INET in i for i in addresses)
-        """
+        """Returns the names of all valid network interface names"""
         names = []
         for name in netifaces.interfaces():
             # only add network interfaces which have IPv4
@@ -259,11 +204,6 @@ class NetworkInfo(object):
         """
         Based on the result from :meth:`ip` return the network interface
         in use
-
-        >>> assert any(
-        ...     i.get("addr") == network.ip()
-        ...     for i in netifaces.ifaddresses(
-        ...         network.interface()).get(socket.AF_INET, []))
         """
         public_address = self.ip()
         for interface in netifaces.interfaces():
@@ -288,12 +228,17 @@ class NetworkInfo(object):
         ... else:
         ...     assert network.dnsip()
         """
-        addr = socket.gethostbyname(self.hostname(fqdn=fqdn))
+        hostname = self.hostname(fqdn=fqdn)
+        addr = socket.gethostbyname(hostname)
+        reverse_name, aliases, addrs = socket.gethostbyaddr(addr)
 
         # addr may not be public and is often the case on linux
         # where the hostname is mapped to 127.0.0.1
-        if self.isPublic(addr):
+        if reverse_name == "localhost":
+            return
+        elif self.isPublic(addr):
             return addr
+
 
     def ip(self):
         """
@@ -303,8 +248,6 @@ class NetworkInfo(object):
             * ask DNS using the fully qualified domain name
             * bind to an external server and check for the address used (does
               not send any data)
-
-        >>> assert network.isPublic(network.ip())
         """
         if self._cached_ip is not None:
             return self._cached_ip
@@ -373,20 +316,11 @@ class ProcessorInfo(object):
         Returns the total number of cpus installed.  This first
         attempts to use :func:`multiprocessing.cpu_count` before
         falling back onto `psutil.NUM_CPUS`
-
-        >>> cpu_count = None
-        >>> if multiprocessing is not None:
-        ...      try:
-        ...          cpu_count = multiprocessing.cpu_count()
-        ...      except:
-        ...          pass
-        >>> cpu_count = psutil.NUM_CPUS if cpu_count is None else cpu_count
-        >>> assert processor.CPU_COUNT == cpu_count
     """
     if multiprocessing is not None:
         try:
             CPU_COUNT = multiprocessing.cpu_count()
-        except Exception, e:
+        except NotImplementedError, e:
             warn("failed to use multiprocess.cpu_count: %s" % e,
                  RuntimeWarning)
 
@@ -396,37 +330,27 @@ class ProcessorInfo(object):
         """
         Returns the load across all cpus value from zero to one.  A value
         of 1.0 means the average load across all cpus is 100%.
-
-        >>> if "TRAVIS" not in os.environ:
-        ...     assert psutil.cpu_percent(.5) / processor.CPU_COUNT > 0
-        ...     assert processor.load(.5) > 0
         """
         return psutil.cpu_percent(iterval) / self.CPU_COUNT
 
-    def user(self):
+    def userTime(self):
         """
         Returns the amount of time spent by the cpu in user
         space
-
-        >>> assert psutil.cpu_times().user <= processor.user()
         """
         return psutil.cpu_times().user
 
-    def system(self):
+    def systemTime(self):
         """
         Returns the amount of time spent by the cpu in system
         space
-
-        >>> assert psutil.cpu_times().system <= processor.system()
         """
         return psutil.cpu_times().system
 
-    def idle(self):
+    def idleTime(self):
         """
         Returns the amount of time spent by the cpu in idle
         space
-
-        >>> assert psutil.cpu_times().idle <= processor.idle()
         """
         return psutil.cpu_times().idle
 
@@ -467,46 +391,26 @@ class MemoryInfo(object):
     :attr TOTAL_RAM:
         Total physical memory (ram) installed on the system
 
-        >>> assert convert.bytetomb(psutil.TOTAL_PHYMEM) == memory.TOTAL_RAM
-
     :attr TOTAL_SWAP:
         Total virtual memory (swap) installed on the system
-
-        >>> assert convert.bytetomb(psutil.swap_memory().total) == memory.TOTAL_SWAP
     """
     TOTAL_RAM = convert.bytetomb(psutil.TOTAL_PHYMEM)
     TOTAL_SWAP = convert.bytetomb(psutil.swap_memory().total)
 
     def swapUsed(self):
-        """
-        Amount of swap currently in use
-
-        >>> assert convert.bytetomb(psutil.swap_memory().used) == memory.swapUsed()
-        """
+        """Amount of swap currently in use"""
         return convert.bytetomb(psutil.swap_memory().used)
 
     def swapFree(self):
-        """
-        Amount of swap currently free
-
-        >>> assert convert.bytetomb(psutil.swap_memory().free) == memory.swapFree()
-        """
+        """Amount of swap currently free"""
         return convert.bytetomb(psutil.swap_memory().free)
 
     def ramUsed(self):
-        """
-        Amount of swap currently free
-
-        >>> assert convert.bytetomb(psutil.virtual_memory().used) == memory.ramUsed()
-        """
+        """Amount of swap currently free"""
         return convert.bytetomb(psutil.virtual_memory().used)
 
     def ramFree(self):
-        """
-        Amount of ram currently free
-
-        >>> assert convert.bytetomb(psutil.virtual_memory().available) == memory.ramFree()
-        """
+        """Amount of ram currently free"""
         return convert.bytetomb(psutil.virtual_memory().available)
 
 
@@ -514,5 +418,4 @@ class MemoryInfo(object):
 memory = MemoryInfo()
 processor = ProcessorInfo()
 network = NetworkInfo()
-filesystem = FileSystemInfo()
 operating_system = OperatingSystemInfo()
---
+++ tests/test_system.py
@@ -0,0 +1,189 @@
+# No shebang line, this module is meant to be imported
+#
+# Copyright 2013 Oliver Palmer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import time
+import socket
+import psutil
+import netifaces
+
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
+try:
+    import pwd
+    _user = lambda: pwd.getpwuid(os.getuid())[0]
+
+except ImportError:
+    import getpass
+    _user = lambda: getpass.getuser()
+
+from nose.tools import eq_
+from nose.plugins.skip import SkipTest
+from pyfarm.ext.utility import convert
+from pyfarm.ext.system import (user, operating_system, network,
+                               processor, memory)
+
+
+def test_user():
+    eq_(user(), _user())
+
+
+def test_os_uptime():
+    t1 = operating_system.uptime()
+    t2 = time.time() - psutil.BOOT_TIME
+    eq_(t2 - t1 < 5, True)
+
+
+def test_net_public():
+    eq_(network.isPublic("0.0.0.0"), False)
+    eq_(network.isPublic("127.0.0.1"), False)
+    eq_(network.isPublic("169.254.0.0"), False)
+    eq_(network.isPublic("10.56.0.0"), True)
+
+
+def test_net_packets_sent():
+    v = psutil.network_io_counters(
+        pernic=True)[network.interface()].packets_sent
+    eq_(network.packetsSent() >= v, True)
+
+
+def test_net_packets_recv():
+    v = psutil.network_io_counters(
+        pernic=True)[network.interface()].packets_recv
+    eq_(network.packetsReceived() >= v, True)
+
+
+def test_net_data_sent():
+    v = convert.bytetomb(psutil.network_io_counters(
+        pernic=True)[network.interface()].bytes_sent)
+    eq_(network.dataSent() >= v, True)
+
+
+def test_net_data_recv():
+    v = convert.bytetomb(psutil.network_io_counters(
+        pernic=True)[network.interface()].bytes_recv)
+    eq_(network.dataReceived() >= v, True)
+
+
+def test_net_error_incoming():
+    v = psutil.network_io_counters(pernic=True)[network.interface()].errin
+    eq_(network.errorCountIncoming() >= v, True)
+
+
+def test_net_error_outgoing():
+    v = psutil.network_io_counters(pernic=True)[network.interface()].errout
+    eq_(network.errorCountOutgoing() >= v, True)
+
+
+def test_net_hostname():
+    eq_(network.hostname(), socket.getfqdn())
+    eq_(network.hostname(fqdn=False), socket.gethostname())
+
+
+def test_net_addresses():
+    eq_(len(network.addresses()) > 1, True)
+    eq_(isinstance(network.addresses(), list), True)
+    eq_(all(network.isPublic(a) for a in network.addresses()), True)
+
+
+def test_net_interfaces():
+    names = network.interfaces()
+    eq_(len(names) > 1, True)
+    eq_(isinstance(names, list), True)
+    eq_(all(name in netifaces.interfaces() for name in names), True)
+
+    addresses = map(netifaces.ifaddresses, names)
+    eq_(all(socket.AF_INET in i for i in addresses), True)
+
+
+def test_net_interface():
+    eq_(any(
+        i.get("addr") == network.ip()
+        for i in netifaces.ifaddresses(
+        network.interface()).get(socket.AF_INET, [])), True)
+
+
+def test_net_ip():
+    eq_(network.isPublic(network.ip()), True)
+
+
+def test_processor_count():
+    count = None
+    if multiprocessing is not None:
+        try:
+            count = multiprocessing.cpu_count()
+        except NotImplementedError:
+            count = None
+
+    if count is None:
+        count = psutil.NUM_CPUS
+
+    eq_(processor.CPU_COUNT, count)
+
+
+def test_processor_load():
+    if "TRAVIS" in os.environ:
+        raise SkipTest
+
+    eq_(psutil.cpu_percent(.5) / processor.CPU_COUNT > 0, True)
+    eq_(processor.load(.5) > 0, True)
+
+
+def test_processor_usertime():
+    eq_(psutil.cpu_times().user <= processor.userTime(), True)
+
+
+def test_processor_systemtime():
+    eq_(psutil.cpu_times().system <= processor.systemTime(), True)
+
+
+def test_processor_idletime():
+    eq_(psutil.cpu_times().idle <= processor.idleTime(), True)
+
+
+def test_memory_totalram():
+    eq_(memory.TOTAL_RAM, convert.bytetomb(psutil.TOTAL_PHYMEM))
+
+
+def test_memory_totalswap():
+    eq_(memory.TOTAL_SWAP, convert.bytetomb(psutil.swap_memory().total))
+
+
+def test_memory_swapused():
+    v1 = convert.bytetomb(psutil.swap_memory().used)
+    v2 = memory.swapUsed()
+    eq_(v1-v2 < 5, True)
+
+
+def test_memory_swapfree():
+    v1 = convert.bytetomb(psutil.swap_memory().free)
+    v2 = memory.swapFree()
+    eq_(v1-v2 < 5, True)
+
+
+def test_memory_ramused():
+    v1 = convert.bytetomb(psutil.virtual_memory().used)
+    v2 = memory.ramUsed()
+    eq_(v1-v2 < 5, True)
+
+
+def test_memory_ramfree():
+    v1 = convert.bytetomb(psutil.virtual_memory().available)
+    v2 = memory.ramFree()
+    eq_(v1-v2 < 5, True)
