1fa93b229b6b3ad7a2a237ba9df09ce5694d71ca
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-29 05:48:39 -0400

    convert to use test case

--- tests/test_system.py
+++ tests/test_system.py
@@ -21,21 +21,7 @@ import psutil
 import tempfile
 import netifaces
 
-try:
-    import multiprocessing
-except ImportError:
-    multiprocessing = None
-
-try:
-    import pwd
-    _user = lambda: pwd.getpwuid(os.getuid())[0]
-
-except ImportError:
-    import getpass
-    _user = lambda: getpass.getuser()
-
-from nose.tools import eq_
-from nose.plugins.skip import SkipTest
+from utcore import TestCase, skip_on_ci
 from pyfarm.ext.utility import convert
 from pyfarm.ext.config.enum import OperatingSystem as _OperatingSystem
 from pyfarm.ext.system import (user, operating_system, network,
@@ -43,178 +29,165 @@ from pyfarm.ext.system import (user, operating_system, network,
 
 osenum = _OperatingSystem()
 
-
-def test_user():
-    eq_(user(), _user())
-
-
-def test_os_uptime():
-    t1 = operating_system.uptime()
-    t2 = time.time() - psutil.BOOT_TIME
-    eq_(t2 - t1 < 5, True)
-
-
-def test_os_classvars():
-    _os = osenum.get()
-    eq_(operating_system.OS, _os)
-    eq_(operating_system.IS_LINUX, _os == osenum.LINUX)
-    eq_(operating_system.IS_WINDOWS, _os == osenum.WINDOWS)
-    eq_(operating_system.IS_MAC, _os == osenum.MAC)
-    eq_(operating_system.IS_OTHER, _os == osenum.OTHER)
-    eq_(operating_system.IS_POSIX, _os in (osenum.LINUX, osenum.MAC))
-
-
-def test_os_case_sensitive():
-    fid, path = tempfile.mkstemp()
-    exists = map(os.path.isfile, [path, path.lower(), path.upper()])
-    if not any(exists):
-        raise ValueError("failed to determine if path was case sensitive")
-    elif all(exists):
-        eq_(operating_system.CASE_SENSITIVE, False)
-    elif exists.count(True) == 1:
-        eq_(operating_system.CASE_SENSITIVE, True)
-
-    try:
-        os.remove(path)
-    except:
-        pass
-
-
-def test_net_packets_sent():
-    v = psutil.network_io_counters(
-        pernic=True)[network.interface()].packets_sent
-    eq_(network.packetsSent() >= v, True)
-
-
-def test_net_packets_recv():
-    v = psutil.network_io_counters(
-        pernic=True)[network.interface()].packets_recv
-    eq_(network.packetsReceived() >= v, True)
-
-
-def test_net_data_sent():
-    v = convert.bytetomb(psutil.network_io_counters(
-        pernic=True)[network.interface()].bytes_sent)
-    eq_(network.dataSent() >= v, True)
-
-
-def test_net_data_recv():
-    v = convert.bytetomb(psutil.network_io_counters(
-        pernic=True)[network.interface()].bytes_recv)
-    eq_(network.dataReceived() >= v, True)
-
-
-def test_net_error_incoming():
-    v = psutil.network_io_counters(pernic=True)[network.interface()].errin
-    eq_(network.errorCountIncoming() >= v, True)
-
-
-def test_net_error_outgoing():
-    v = psutil.network_io_counters(pernic=True)[network.interface()].errout
-    eq_(network.errorCountOutgoing() >= v, True)
-
-
-def test_net_hostname():
-    eq_(network.hostname(), socket.getfqdn())
-    eq_(network.hostname(fqdn=False), socket.gethostname())
-
-
-def test_net_addresses():
-    eq_(len(network.addresses()) >= 1, True)
-    eq_(isinstance(network.addresses(), list), True)
-    eq_(all(network.isPublic(a) for a in network.addresses()), True)
-
-
-def test_net_interfaces():
-    names = network.interfaces()
-    eq_(len(names) > 1, True)
-    eq_(isinstance(names, list), True)
-    eq_(all(name in netifaces.interfaces() for name in names), True)
-
-    addresses = map(netifaces.ifaddresses, names)
-    eq_(all(socket.AF_INET in i for i in addresses), True)
-
-
-def test_net_interface():
-    eq_(any(
-        i.get("addr") == network.ip()
-        for i in netifaces.ifaddresses(
-        network.interface()).get(socket.AF_INET, [])), True)
-
-
-def test_net_ip():
-    eq_(network.isPublic(network.ip()), True)
-
-
-def test_processor_count():
-    count = None
-    if multiprocessing is not None:
+class OperatingSystem(TestCase):
+    def test_user(self):
         try:
-            count = multiprocessing.cpu_count()
-        except NotImplementedError:
-            count = None
+            import pwd
+            _user = lambda: pwd.getpwuid(os.getuid())[0]
 
-    if count is None:
-        count = psutil.NUM_CPUS
+        except ImportError:
+            import getpass
+            _user = lambda: getpass.getuser()
 
-    eq_(processor.CPU_COUNT, count)
+        self.assertEqual(user(), _user())
+
+    def test_uptime(self):
+        t1 = operating_system.uptime()
+        t2 = time.time() - psutil.BOOT_TIME
+        self.assertEqual(t2 - t1 < 5, True)
+
+    def test_classvars(self):
+        _os = osenum.get()
+        self.assertEqual(operating_system.OS, _os)
+        self.assertEqual(operating_system.IS_LINUX, _os == osenum.LINUX)
+        self.assertEqual(operating_system.IS_WINDOWS, _os == osenum.WINDOWS)
+        self.assertEqual(operating_system.IS_MAC, _os == osenum.MAC)
+        self.assertEqual(operating_system.IS_OTHER, _os == osenum.OTHER)
+        self.assertEqual(operating_system.IS_POSIX,
+                         _os in (osenum.LINUX, osenum.MAC))
+
+    def test_case_sensitive(self):
+        fid, path = tempfile.mkstemp()
+        exists = map(os.path.isfile, [path, path.lower(), path.upper()])
+        if not any(exists):
+            raise ValueError("failed to determine if path was case sensitive")
+        elif all(exists):
+            self.assertEqual(operating_system.CASE_SENSITIVE, False)
+        elif exists.count(True) == 1:
+            self.assertEqual(operating_system.CASE_SENSITIVE, True)
+
+        self.remove(path)
+
+class Network(TestCase):
+    def test_packets_sent(self):
+        v = psutil.network_io_counters(
+            pernic=True)[network.interface()].packets_sent
+        self.assertEqual(network.packetsSent() >= v, True)
+
+    def test_packets_recv(self):
+        v = psutil.network_io_counters(
+            pernic=True)[network.interface()].packets_recv
+        self.assertEqual(network.packetsReceived() >= v, True)
+
+    def test_data_sent(self):
+        v = convert.bytetomb(psutil.network_io_counters(
+            pernic=True)[network.interface()].bytes_sent)
+        self.assertEqual(network.dataSent() >= v, True)
+
+    def test_data_recv(self):
+        v = convert.bytetomb(psutil.network_io_counters(
+            pernic=True)[network.interface()].bytes_recv)
+        self.assertEqual(network.dataReceived() >= v, True)
+
+    def test_error_incoming(self):
+        v = psutil.network_io_counters(pernic=True)[network.interface()].errin
+        self.assertEqual(network.errorCountIncoming() >= v, True)
+
+    def test_error_outgoing(self):
+        v = psutil.network_io_counters(pernic=True)[network.interface()].errout
+        self.assertEqual(network.errorCountOutgoing() >= v, True)
+
+    def test_hostname(self):
+        self.assertEqual(network.hostname(), socket.getfqdn())
+        self.assertEqual(network.hostname(fqdn=False), socket.gethostname())
+
+    def test_addresses(self):
+        self.assertEqual(len(network.addresses()) >= 1, True)
+        self.assertEqual(isinstance(network.addresses(), list), True)
+        self.assertEqual(all(network.isPublic(a) for a in network.addresses()),
+                         True)
+
+    def test_interfaces(self):
+        names = network.interfaces()
+        self.assertEqual(len(names) > 1, True)
+        self.assertEqual(isinstance(names, list), True)
+        self.assertEqual(all(name in netifaces.interfaces() for name in names),
+                         True)
+
+        addresses = map(netifaces.ifaddresses, names)
+        self.assertEqual(all(socket.AF_INET in i for i in addresses), True)
+
+    def test_interface(self):
+        self.assertEqual(any(
+            i.get("addr") == network.ip()
+            for i in netifaces.ifaddresses(
+            network.interface()).get(socket.AF_INET, [])), True)
+
+    def test_ip(self):
+        self.assertEqual(network.isPublic(network.ip()), True)
 
 
-def test_processor_load():
-    # skip for now, add things happen when running on a vm
-    if "TRAVIS" in os.environ or "BUILDBOT_UUID" in os.environ:
-        raise SkipTest
+class Processor(TestCase):
+    def test_count(self):
+        try:
+            import multiprocessing
+            cpu_count = multiprocessing.cpu_count()
+        except (ImportError, NotImplementedError):
+            cpu_count = psutil.NUM_CPUS
 
-    eq_(psutil.cpu_percent(.5) / processor.CPU_COUNT >= 0, True)
-    eq_(processor.load(.5) > 0, True)
+        self.assertEqual(processor.CPU_COUNT, cpu_count)
 
+    @skip_on_ci
+    def test_load(self):
+        self.assertEqual(psutil.cpu_percent(.5) / processor.CPU_COUNT >= 0,
+                         True)
+        self.assertEqual(processor.load(.5) > 0, True)
 
-def test_processor_usertime():
-    eq_(psutil.cpu_times().user <= processor.userTime(), True)
+    def test_usertime(self):
+        self.assertEqual(psutil.cpu_times().user <= processor.userTime(),
+                         True)
 
+    def test_systemtime(self):
+        self.assertEqual(psutil.cpu_times().system <= processor.systemTime(),
+                         True)
 
-def test_processor_systemtime():
-    eq_(psutil.cpu_times().system <= processor.systemTime(), True)
+    def test_idletime(self):
+        self.assertEqual(psutil.cpu_times().idle <= processor.idleTime(),
+                         True)
 
-
-def test_processor_idletime():
-    eq_(psutil.cpu_times().idle <= processor.idleTime(), True)
-
-
-def test_processor_iowait():
-    if operating_system.IS_LINUX:
-        eq_(processor.iowait() <= psutil.cpu_times().iowait, True)
-    else:
-        eq_(processor.iowait(), None)
-
-
-def test_memory_totalram():
-    eq_(memory.TOTAL_RAM, convert.bytetomb(psutil.TOTAL_PHYMEM))
-
-
-def test_memory_totalswap():
-    eq_(memory.TOTAL_SWAP, convert.bytetomb(psutil.swap_memory().total))
-
-
-def test_memory_swapused():
-    v1 = convert.bytetomb(psutil.swap_memory().used)
-    v2 = memory.swapUsed()
-    eq_(v1-v2 < 5, True)
+    def test_iowait(self):
+        if operating_system.IS_LINUX:
+            self.assertEqual(processor.iowait() <= psutil.cpu_times().iowait,
+                             True)
+        else:
+            self.assertEqual(processor.iowait(), None)
 
 
-def test_memory_swapfree():
-    v1 = convert.bytetomb(psutil.swap_memory().free)
-    v2 = memory.swapFree()
-    eq_(v1-v2 < 5, True)
+class Memory(TestCase):
+    def test_totalram(self):
+        self.assertEqual(memory.TOTAL_RAM,
+                         convert.bytetomb(psutil.TOTAL_PHYMEM))
 
+    def test_totalswap(self):
+        self.assertEqual(memory.TOTAL_SWAP,
+                         convert.bytetomb(psutil.swap_memory().total))
 
-def test_memory_ramused():
-    v1 = convert.bytetomb(psutil.virtual_memory().used)
-    v2 = memory.ramUsed()
-    eq_(v1-v2 < 5, True)
+    def test_swapused(self):
+        v1 = convert.bytetomb(psutil.swap_memory().used)
+        v2 = memory.swapUsed()
+        self.assertEqual(v1-v2 < 5, True)
 
+    def test_swapfree(self):
+        v1 = convert.bytetomb(psutil.swap_memory().free)
+        v2 = memory.swapFree()
+        self.assertEqual(v1-v2 < 5, True)
 
-def test_memory_ramfree():
-    v1 = convert.bytetomb(psutil.virtual_memory().available)
-    v2 = memory.ramFree()
-    eq_(v1-v2 < 5, True)
+    def test_ramused(self):
+        v1 = convert.bytetomb(psutil.virtual_memory().used)
+        v2 = memory.ramUsed()
+        self.assertEqual(v1-v2 < 5, True)
+
+    def test_ramfree(self):
+        v1 = convert.bytetomb(psutil.virtual_memory().available)
+        v2 = memory.ramFree()
+        self.assertEqual(v1-v2 < 5, True)
