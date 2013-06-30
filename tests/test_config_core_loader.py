58b91f276ca13954b2a986ee46b26fe7bc44b649
Oliver Palmer <oliverpalmer@opalmer.com>
2013-06-30 19:17:38 -0400

    moved over to class based tests

--- tests/test_config_core_loader.py
+++ tests/test_config_core_loader.py
@@ -23,222 +23,204 @@ try:
 except ImportError:
     from ordereddict import OrderedDict
 
-from nose.tools import raises, with_setup, eq_
+from nose.tools import raises
 
 from pyfarm.files import yamlDump
 from pyfarm import error
+from utcore import TestCase
 from pyfarm.ext.config.core.find import configFiles
 from pyfarm.ext.config.core.loader import Loader
-from tests.pyfarmnose import (
-    pretest_cleanup_env, posttest_cleanup_files, mktmps, mktmp
-)
 
 
-def prerun():
-    pretest_cleanup_env()
-    Loader._DATA.clear()
+class ConfigTestBase(TestCase):
+    def _generatedata(self):
+        data = OrderedDict()
+        user = self.mktempdir()
+        roots = self.mktempdir()
+        system = self.mktempdir()
+        kwargs = {"user": user, "roots": roots, "system": system}
 
-envsetup = with_setup(
-    setup=prerun,
-    teardown=posttest_cleanup_files
-)
+        for dirname in [user, roots, system]:
+            filepath = os.path.join(dirname, "config.yml")
+            filedata = {str(uuid.uuid4()): str(uuid.uuid4()), "test": True}
+            yamlDump(filedata, filepath)
+            data[filepath] = filedata
 
-
-def _generatedata():
-    data = OrderedDict()
-    user, roots, system = mktmps(3)
-    kwargs = {"user": user, "roots": roots, "system": system}
-
-    for dirname in chain(*[user, roots, system]):
-        filepath = os.path.join(dirname, "config.yml")
-        filedata = {str(uuid.uuid4()): str(uuid.uuid4()), "test" : True}
-        yamlDump(filedata, filepath)
-        data[filepath] = filedata
-
-    return data, kwargs
+        return data, kwargs
 
 
-@raises(error.PreferencesNotFoundError)
-def test_raises_notfound():
-    Loader("")
+
+class TestErrors(ConfigTestBase):
+    def setUp(self):
+        super(TestErrors, self).setUp()
+        Loader._DATA.clear()
 
 
-@raises(AssertionError)
-def test_assert_invalid_dict_type():
-    Loader(data="")
+    @raises(error.PreferencesNotFoundError)
+    def test__notfound(self):
+        Loader("")
 
 
-@raises(AssertionError)
-def test_assert_invalid_typename_type():
-    Loader(filename={})
+    @raises(AssertionError)
+    def test_invalid_dict_type(self):
+        Loader(data="")
 
 
-@envsetup
-def test_load_files():
-    data, kwargs = _generatedata()
-    config = Loader("config.yml", **kwargs)
-    eq_(config.files, configFiles("config.yml", **kwargs))
+    @raises(AssertionError)
+    def test_invalid_typename_type(self):
+        Loader(filename={})
 
 
-@envsetup
-def test_data():
-    uuid_data = {}
-    data, kwargs = _generatedata()
-    map(uuid_data.update, data.itervalues())
-    config = Loader("config.yml", **kwargs)
-
-    for key, value in config.iteritems():
-        eq_(uuid_data[key], value)
+    @raises(KeyError)
+    def test_setitem_postinstance(self):
+        config = Loader("", data={}, load=False)
+        config["foo"] = True
+        config["foo"]
 
 
-@envsetup
-def test_where_unique():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
-
-    # generate unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {str(uuid.uuid4()): str(uuid.uuid4())}
-        data[yamlDump(d, name)] = d
-
-    config = Loader("config.yml", roots=dirs)
-    for filename, data in data.iteritems():
-        eq_(config.where(data.keys()[0])[0], filename)
+    @raises(KeyError)
+    def test_supdate_postinstance(self):
+        config = Loader("", data={}, load=False)
+        config.update({"foo": True})
+        config["foo"]
 
 
-@envsetup
-def test_where_nonunique():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
+class General(ConfigTestBase):
+    def test_load_files(self):
+        data, kwargs = self._generatedata()
+        config = Loader("config.yml", **kwargs)
+        self.assertEqual(config.files, configFiles("config.yml", **kwargs))
 
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {"test": str(uuid.uuid4())}
-        data[yamlDump(d, name)] = d
+    def test_data(self):
+        uuid_data = {}
+        data, kwargs = self._generatedata()
+        map(uuid_data.update, data.itervalues())
+        config = Loader("config.yml", **kwargs)
 
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.where("test"), config.files)
-
-
-@envsetup
-def test_where_unknown():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
-
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {}
-        data[yamlDump(d, name)] = d
-
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.where("foo"), [])
+        for key, value in config.iteritems():
+            self.assertEqual(uuid_data[key], value)
 
 
-@envsetup
-def test_which_unique():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
+class TestWhere(ConfigTestBase):
+    def test_unique(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
-    # generate unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {str(uuid.uuid4()): str(uuid.uuid4())}
-        data[yamlDump(d, name)] = d
+        # generate unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {str(uuid.uuid4()): str(uuid.uuid4())}
+            data[yamlDump(d, name)] = d
 
-    config = Loader("config.yml", roots=dirs)
-    for filename, data in data.iteritems():
-        eq_(config.which(data.keys()[0]), filename)
+        config = Loader("config.yml", roots=dirs)
+        for filename, data in data.iteritems():
+            self.assertEqual(config.where(data.keys()[0])[0], filename)
 
+    def test_nonunique(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
-@envsetup
-def test_which_nonunique():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {"test": str(uuid.uuid4())}
+            data[yamlDump(d, name)] = d
 
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {"test": str(uuid.uuid4())}
-        data[yamlDump(d, name)] = d
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.where("test"), config.files)
 
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.which("test"), config.files[-1])
+    def test_unknown(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {}
+            data[yamlDump(d, name)] = d
 
-@envsetup
-def test_which_unknown():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
-
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {}
-        data[yamlDump(d, name)] = d
-
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.which("foo"), None)
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.where("foo"), [])
 
 
-@envsetup
-def test_get_override():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
+class TestWhich(TestCase):
+    def test_unique(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        d = {"test": str(uuid.uuid4())}
-        data[yamlDump(d, name)] = d
-        last_value = d.values()[0]
+        # generate unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {str(uuid.uuid4()): str(uuid.uuid4())}
+            data[yamlDump(d, name)] = d
 
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.get("test"), last_value)
+        config = Loader("config.yml", roots=dirs)
+        for filename, data in data.iteritems():
+            self.assertEqual(config.which(data.keys()[0]), filename)
 
+    def test_nonunique(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
-@envsetup
-def test_get_dot_syntax():
-    dirs = [mktmp() for _ in xrange(3)]
-    filenames = [os.path.join(d, "config.yml") for d in dirs]
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {"test": str(uuid.uuid4())}
+            data[yamlDump(d, name)] = d
 
-    # generate non-unique data
-    data = OrderedDict()
-    for name in filenames:
-        last_value = str(uuid.uuid4())
-        d = {"test": {"foo": last_value}}
-        data[yamlDump(d, name)] = d
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.which("test"), config.files[-1])
 
-    config = Loader("config.yml", roots=dirs)
-    eq_(config.get("test.foo"), last_value)
+    def test_unknown(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
 
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {}
+            data[yamlDump(d, name)] = d
 
-@envsetup
-def test_get_unknown():
-    config = Loader("", data={}, load=False)
-    eq_(config.get("", -1), -1)
-
-
-@envsetup
-def test_get_known():
-    v = str(uuid.uuid4())
-    config = Loader("", data={"test": v}, load=False)
-    eq_(config.get("test"), v)
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.which("foo"), None)
 
 
-@raises(KeyError)
-@envsetup
-def test_setitem_postinstance():
-    config = Loader("", data={}, load=False)
-    config["foo"] = True
-    config["foo"]
+class TestGet(ConfigTestBase):
+    def test_override(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
+        last_value = None
+    
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            d = {"test": str(uuid.uuid4())}
+            data[yamlDump(d, name)] = d
+            last_value = d.values()[0]
+    
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.get("test"), last_value)
+    
+    def test_dot_syntax(self):
+        dirs = [self.mktempdir() for _ in xrange(3)]
+        filenames = [os.path.join(d, "config.yml") for d in dirs]
+        last_value = None
+    
+        # generate non-unique data
+        data = OrderedDict()
+        for name in filenames:
+            last_value = str(uuid.uuid4())
+            d = {"test": {"foo": last_value}}
+            data[yamlDump(d, name)] = d
+    
+        config = Loader("config.yml", roots=dirs)
+        self.assertEqual(config.get("test.foo"), last_value)
 
+    def test_unknown(self):
+        config = Loader("", data={}, load=False)
+        self.assertEqual(config.get("", -1), -1)
 
-@raises(KeyError)
-@envsetup
-def test_supdate_postinstance():
-    config = Loader("", data={}, load=False)
-    config.update({"foo": True})
-    config["foo"]
+    def test_known(self):
+        v = str(uuid.uuid4())
+        config = Loader("", data={"test": v}, load=False)
+        self.assertEqual(config.get("test"), v)
