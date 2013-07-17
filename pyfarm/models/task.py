c5fb55c0fbf6028431bb5b6345daeff2a8114ed7
Oliver Palmer <oliverpalmer@opalmer.com>
2013-07-16 23:10:21 -0400

    initial wrapper model

--- pyfarm/models/task.py
+++ pyfarm/models/task.py
@@ -22,6 +22,7 @@ from pyfarm.config.enum import WorkState
 from pyfarm.utility import randint
 from pyfarm.models.constants import DBCFG, TABLE_JOB, TABLE_TASK, TABLE_AGENT
 from pyfarm.models.mixins import StateValidationMixin, StateChangedMixin
+from pyfarm.models import modelfor
 
 
 class TaskModel(db.Model, StateValidationMixin, StateChangedMixin):
@@ -38,7 +39,7 @@ class TaskModel(db.Model, StateValidationMixin, StateChangedMixin):
     priority = db.Column(db.Integer, default=DBCFG.get("task.priority"),
                          nullable=False)
     attempts = db.Column(db.Integer, default=0)
-    frame = db.Column(db.Integer)
+    frame = db.Column(db.Integer, nullable=False)
 
     # time information
     time_submitted = db.Column(db.DateTime, default=datetime.now)
@@ -58,6 +59,65 @@ class TaskModel(db.Model, StateValidationMixin, StateChangedMixin):
         if new_value is not None:
             target.state = target.STATE_ENUM.ASSIGN
 
-
 event.listen(TaskModel._agentid, "set", TaskModel.agentChangedEvent)
 event.listen(TaskModel.state, "set", TaskModel.stateChangedEvent)
+
+
+class Task(TaskModel):
+    """
+    Provides :meth:`__init__` for :class:`TaskModel` so the model can
+    be instanced with initial values.
+    """
+    def __init__(self, job, frame, parent_task=None, state=None,
+                 priority=None, attempts=None, agent=None):
+        # build parent job id
+        if not modelfor(job, TABLE_JOB):
+            jobid = job.jobid
+            if jobid is None:
+                raise ValueError("`job` with null id provided")
+        elif isinstance(job, long):
+            jobid = job
+        else:
+            raise ValueError("failed to determine job id")
+
+        # build parent task id
+        if parent_task is None:
+            parent_taskid = None
+        elif modelfor(parent_task, TABLE_TASK):
+            parent_taskid = parent_task.id
+            if parent_taskid is None:
+                raise ValueError("`parent_task` with null id provided")
+        elif isinstance(parent_task, long):
+            parent_taskid = parent_task
+        else:
+            raise ValueError("failed to determine parent task id")
+
+        # build agent id
+        if agent is None:
+            agentid = None
+        elif modelfor(agent, TABLE_AGENT):
+            agentid = agent.id
+            if agentid is None:
+                raise ValueError("`agent` with null id provided")
+        elif isinstance(agent, long):
+            agentid = agent
+        else:
+            raise ValueError("failed to determine agent id")
+
+        self._jobid = jobid
+        self.frame = frame
+
+        if parent_taskid is not None:
+            self._parenttask = parent_taskid
+
+        if agentid is not None:
+            self._agentid = agentid
+
+        if state is not None:
+            self.state = state
+
+        if priority is not None:
+            self.priority = priority
+
+        if attempts is None:
+            self.attempts = attempts
