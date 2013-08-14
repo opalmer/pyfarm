1f90a12781ce4e2af3dfeb3db8f04f09ef1e1a71
Oliver Palmer <oliverpalmer@opalmer.com>
2013-08-13 23:15:13 -0400

    added docs

--- pyfarm/models/core/cfg.py
+++ pyfarm/models/core/cfg.py
@@ -14,6 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Stores basic configuration data related to tables and models.
+
+:var DBCFG: Instanced
+    :class:`Loader` class which loads dbdata.yml
+
+:var TABLE_PREFIX:
+    Prefix for all tables, defaults to dbdata.yml:tables.prefix
+
+:var TABLE_AGENT:
+    Stores the name of the table for agents
+
+:var TABLE_AGENT_TAGS:
+    Stores the name of the table for agent tags
+
+:var TABLE_AGENT_SOFTWARE:
+    Stores the name of the table for agent software
+
+:var TABLE_JOB:
+    Stores the name of the table for jobs
+
+:var TABLE_JOB_TAGS:
+    Stores the name of the table for job tags
+
+:var TABLE_JOB_SOFTWARE:
+    Stores the name of the table for job software
+
+:var TABLE_TASK:
+    Stores the name of the table for job tasks
+"""
+
 from pyfarm.ext.config.core.loader import Loader
 
 DBCFG = Loader("dbdata.yml")
