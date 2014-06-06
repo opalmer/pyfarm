c3933ae03515a11d08a36031820e8a9c17ca395f
opalmer <oliverpalmer+github@opalmer.com>
2014-06-05 22:47:06 -0400

    adding retry.sh to retry commands in travis

---
+++ misc/retry.sh
@@ -0,0 +1,19 @@
+function retry {
+    local attempt=1
+    local command="${@}" 
+
+    while [ $attempt -ne 10 ]; do
+        echo "(run $attempt/10) running $command"
+        $command
+        if [[ $? -eq 0 ]]; then
+            echo "(success) $command"
+            break
+        else
+            ((attempt++))
+            echo "(FAILED) $command"
+            sleep 3
+        fi
+    done
+}
+
+retry $*
